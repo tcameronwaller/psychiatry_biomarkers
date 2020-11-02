7ba98a0c55ad50674da7a9d7d0a56afaa3ebb114
T. Cameron Waller <tcameronwaller@gmail.com>
2020-11-02 11:58:07 -0600

    initialized scripts and package

---
+++ package/__init__.py
@@ -0,0 +1,27 @@
+"""
+Package to organize analyses on genetics of metabolism in psychiatric
+disorders.
+
+Title:
+    bipolar_metabolism
+
+Subpackages:
+    promiscuity
+
+Modules:
+
+"""
+
+###############################################################################
+# Notes
+
+# Recommendation is to introduce promiscuity as a subpackage.
+
+###############################################################################
+# Installation and importation
+
+# Standard.
+
+# Relevant.
+
+# Custom.
---
+++ scripts/restore_repository.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+
+#chmod u+x script.sh
+
+# Read private, local file paths.
+echo "read private file path variables..."
+cd ~/paths
+path_temporary=$(<"./temporary_bipolar_metabolism.txt")
+path_waller="$path_temporary/waller"
+path_bipolar_metabolism="$path_waller/bipolar_metabolism"
+path_promiscuity="$path_waller/promiscuity"
+
+# Echo each command to console.
+set -x
+
+# Remove previous version of program.
+
+echo "remove previous versions of the repositories..."
+rm -r $path_bipolar_metabolism
+rm -r $path_promiscuity
+
+# Access current version of the program.
+
+echo "access current version of the bipolar_metabolism repository..."
+cd $path_waller
+wget https://github.com/tcameronwaller/bipolar_metabolism/archive/main.zip
+unzip main.zip
+rm main.zip
+mv bipolar_metabolism-main $path_sexy_alcohol
+mv "$path_bipolar_metabolism/package" "$path_bipolar_metabolism/bipolar_metabolism"
+
+echo "access current version of the promiscuity repository..."
+cd $path_waller
+wget https://github.com/tcameronwaller/promiscuity/archive/main.zip
+unzip main.zip
+rm main.zip
+mv promiscuity-main $path_promiscuity
+mv "$path_promiscuity/package" "$path_promiscuity/promiscuity"
+
+echo "organize promiscuity as a subpackage within bipolar_metabolism..."
+cp -r "$path_promiscuity/promiscuity" "$path_bipolar_metabolism/bipolar_metabolism/promiscuity"
