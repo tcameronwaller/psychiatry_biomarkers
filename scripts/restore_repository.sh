993d884d1d7b4e2143e6cfae06f6834f30b3f044
T. Cameron Waller <tcameronwaller@gmail.com>
2020-12-28 18:00:31 -0600

    update restore to include uk_biobank sub-repository

--- scripts/restore_repository.sh
+++ scripts/restore_repository.sh
@@ -8,6 +8,7 @@ cd ~/paths
 path_temporary=$(<"./temporary_bipolar_metabolism.txt")
 path_waller="$path_temporary/waller"
 path_bipolar_metabolism="$path_waller/bipolar_metabolism"
+path_uk_biobank="$path_waller/uk_biobank"
 path_promiscuity="$path_waller/promiscuity"
 
 # Echo each command to console.
@@ -17,9 +18,11 @@ set -x
 
 echo "remove previous versions of the repositories..."
 rm -r $path_bipolar_metabolism
+rm -r $path_uk_biobank
 rm -r $path_promiscuity
 
-# Access current version of the program.
+##########
+# Access and organize current version of the main repository.
 
 echo "access current version of the bipolar_metabolism repository..."
 cd $path_waller
@@ -29,6 +32,35 @@ rm main.zip
 mv bipolar_metabolism-main $path_bipolar_metabolism
 mv "$path_bipolar_metabolism/package" "$path_bipolar_metabolism/bipolar_metabolism"
 
+
+
+##########
+# Organize and restore parameters.
+
+# TODO: copy the "parameters" directory from the repository to the "dock"...
+# TODO: modify the access procedure to copy this parameters information to dock, also...
+# Transfer new version of parameters.
+#rm $path_access_table_variables
+#cp $path_parameters_table_variables $path_access_table_variables
+
+##########
+# Organize and restore supplemental sub-repositories.
+
+# Repository: uk_biobank
+# Scripts remain within original repository's structure.
+# Python code transfers to a sub-package within main package.
+echo "access current version of the uk_biobank repository..."
+cd $path_waller
+wget https://github.com/tcameronwaller/uk_biobank/archive/main.zip
+unzip main.zip
+rm main.zip
+mv uk_biobank-main $path_uk_biobank
+mv "$path_uk_biobank/package" "$path_uk_biobank/uk_biobank"
+cp -r "$path_uk_biobank/uk_biobank" "$path_bipolar_metabolism/bipolar_metabolism/uk_biobank"
+
+# Repository: promiscuity
+# Scripts remain within original repository's structure.
+# Python code transfers to sub-package.
 echo "access current version of the promiscuity repository..."
 cd $path_waller
 wget https://github.com/tcameronwaller/promiscuity/archive/main.zip
@@ -36,6 +68,4 @@ unzip main.zip
 rm main.zip
 mv promiscuity-main $path_promiscuity
 mv "$path_promiscuity/package" "$path_promiscuity/promiscuity"
-
-echo "organize promiscuity as a subpackage within bipolar_metabolism..."
 cp -r "$path_promiscuity/promiscuity" "$path_bipolar_metabolism/bipolar_metabolism/promiscuity"
