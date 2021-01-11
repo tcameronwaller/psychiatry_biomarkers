7cd24eb3a9efed1a84905392b25c44484d56db4b
T. Cameron Waller <tcameronwaller@gmail.com>
2021-01-11 11:15:21 -0600

    begin script to copy over metabolite files

---
+++ scripts/metabolite_gene_scores/access_metabolite_gene_scores.sh
@@ -0,0 +1,48 @@
+#!/bin/bash
+
+#chmod u+x script.sh
+
+###########################################################################
+###########################################################################
+###########################################################################
+# This script organizes paths and parameters to access local information
+# about persons and phenotypes in the UKBiobank.
+###########################################################################
+###########################################################################
+###########################################################################
+
+###########################################################################
+# Read private, local file paths.
+echo "read private file path variables and organize paths..."
+cd ~/paths
+path_ukbiobank_metabolites=$(<"./batzler_ukbiobank_metabolites.txt")
+path_temporary=$(<"./temporary_bipolar_metabolism.txt")
+path_dock="$path_temporary/waller/dock"
+path_access_metabolites="$path_dock/access/ukbiobank_metabolites"
+
+# Echo each command to console.
+#set -x
+# Suppress echo each command to console.
+set +x
+
+# Determine whether the temporary directory structure already exists.
+if [ ! -d $path_access_metabolites ]; then
+    # Directory does not already exist.
+    # Create directory.
+    mkdir -p $path_access_metabolites
+fi
+
+# Define glob pattern to recognize relevant files.
+prefix="UKB_M[:digit:][:digit:][:digit:][:digit:][:digit:]"
+pattern="$path_ukbiobank_metabolites/$prefix.metal.pos_imp0.8_maf0.01.all.score.gz"
+# Iterate on all files and directories in parent directory.
+for file in $path_ukbiobank_metabolites/*; do
+  if [ -f "$file" ]; then
+    # Current content item is a file.
+    if [[ $file == $pattern ]]; then
+      # File name matches glob pattern.
+      echo $file
+      # Copy the file to new directory...
+    fi
+  fi
+done
