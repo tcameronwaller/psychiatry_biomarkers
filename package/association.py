ee9f2e682de7370f1667d88312d2c1119b051a90
T. Cameron Waller <tcameronwaller@gmail.com>
2021-02-08 16:48:25 -0600

    scale up to all metabolites

--- package/association.py
+++ package/association.py
@@ -989,7 +989,7 @@ def execute_procedure(
 
     utility.print_terminal_partition(level=1)
     print(path_dock)
-    print("version check: 10")
+    print("version check: 11")
     # Pause procedure.
     time.sleep(5.0)
 
@@ -1012,8 +1012,8 @@ def execute_procedure(
     # M32315: serine
     # M02342: serotonin
     # M00054: tryptophan
-    metabolites = ["M00599", "M32315", "M02342", "M00054"]
-    #metabolites = copy.deepcopy(source["metabolites_valid"])
+    #metabolites = ["M00599", "M32315", "M02342", "M00054"]
+    metabolites = copy.deepcopy(source["metabolites_valid"])
     phenotype="body_mass_index" # "testosterone", "audit_c",
     pail_association = (
         organize_regress_metabolites_genetic_scores_against_phenotypes(
