a18725c7082dad0aa1746d73576d637279105b58
T. Cameron Waller <tcameronwaller@gmail.com>
2021-02-22 15:33:58 -0600

    begin script to access GWAS summary statistics

---
+++ scripts/access/access_gwas_metabolite_summaries.sh
@@ -0,0 +1,46 @@
+#!/bin/bash
+
+#chmod u+x script.sh
+
+###########################################################################
+###########################################################################
+###########################################################################
+# This script accesses GWAS summary statistics from repositories for
+# multiple studies.
+###########################################################################
+###########################################################################
+###########################################################################
+
+###########################################################################
+# Read private, local file paths.
+echo "read private file path variables and organize paths..."
+cd ~/paths
+path_temporary=$(<"./processing_bipolar_metabolism.txt")
+path_dock="$path_temporary/waller/dock"
+path_access="$path_dock/access/gwas_summaries"
+
+# Echo each command to console.
+#set -x
+# Suppress echo each command to console.
+set +x
+
+# Determine whether the temporary directory structure already exists.
+if [ ! -d $path_access ]; then
+    # Directory does not already exist.
+    # Create directory.
+    mkdir -p $path_access
+    mkdir -p $path_access_study
+fi
+
+# Panyard et al, Communications Biology, 2021 (PubMed:33437055)
+if true: then
+  # Define file destination.
+  path_access_study="$path_dock/access/gwas_summaries/33437055_panyard_2021"
+  # Download files from server.
+  # curl -u user:password "path-to-original-file-online" -o "path-to-new-local-copy"
+  cd $path_access
+  wget -r -l 0 "ftp://ftp.biostat.wisc.edu/pub/lu_group/Projects/MWAS/Panyard_GWAS_sumstats_CSF_metabolites/"
+
+  #cp -r "${path_access}/ftp.biostat.wisc.edu/pub/lu_group/Projects/MWAS/Panyard_GWAS_sumstats_CSF_metabolites" \
+  #$path_access
+fi
