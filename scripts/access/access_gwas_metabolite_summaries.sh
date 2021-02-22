53a73098132444843ad1eb2a94cb1285cdae0a41
T. Cameron Waller <tcameronwaller@gmail.com>
2021-02-22 15:48:20 -0600

    sequence error

--- scripts/access/access_gwas_metabolite_summaries.sh
+++ scripts/access/access_gwas_metabolite_summaries.sh
@@ -29,13 +29,13 @@ if [ ! -d $path_access ]; then
     # Directory does not already exist.
     # Create directory.
     mkdir -p $path_access
-    mkdir -p $path_access_study
 fi
 
 # Panyard et al, Communications Biology, 2021 (PubMed:33437055)
 if true: then
   # Define file destination.
   path_access_study="$path_dock/access/gwas_summaries/33437055_panyard_2021"
+  mkdir -p $path_access_study
   # Download files from server.
   # curl -u user:password "path-to-original-file-online" -o "path-to-new-local-copy"
   cd $path_access
@@ -43,4 +43,6 @@ if true: then
 
   #cp -r "${path_access}/ftp.biostat.wisc.edu/pub/lu_group/Projects/MWAS/Panyard_GWAS_sumstats_CSF_metabolites" \
   #$path_access
+
+  # Metabolite information is in Supplemental Table 2.
 fi
