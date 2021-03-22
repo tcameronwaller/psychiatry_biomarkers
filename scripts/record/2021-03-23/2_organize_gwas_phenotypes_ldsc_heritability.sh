e3486b7c33496478f1b6e30e1ec080fc70a6ef16
T. Cameron Waller <tcameronwaller@gmail.com>
2021-03-22 16:28:41 -0500

    ready for next phenotype study

--- scripts/record/2021-03-23/2_organize_gwas_phenotypes_ldsc_heritability.sh
+++ scripts/record/2021-03-23/2_organize_gwas_phenotypes_ldsc_heritability.sh
@@ -31,9 +31,9 @@ path_genetic_correlation="$path_dock/genetic_correlation"
 # Execute procedure.
 
 # Initialize directories.
-rm -r $path_gwas
-rm -r $path_heritability
-rm -r $path_genetic_correlation
+#rm -r $path_gwas
+#rm -r $path_heritability
+#rm -r $path_genetic_correlation
 if [ ! -d $path_gwas ]; then
     # Directory does not already exist.
     # Create directory.
@@ -52,42 +52,46 @@ fi
 
 # Organize information in format for LDSC.
 
-# Yengo et al, Human Molecular Genetics, 2018 (PubMed:30124842)
-# phenotype: body mass index
-study="30124842_yengo_2018"
-source_file="Meta-analysis_Locke_et_al+UKBiobank_2018_UPDATED.txt.gz"
-path_source_directory="${path_gwas_summaries}/${study}"
-path_source_file="${path_source_directory}/${source_file}"
-path_script_gwas_organization="${path_scripts_organization}/organize_gwas_ldsc_${study}.sh"
-report="true" # "true" or "false"
-/usr/bin/bash "$path_scripts_record/3_organize_gwas_phenotype_ldsc_heritability.sh" \
-$study \
-$source_file \
-$path_source_file \
-$path_genetic_reference \
-$path_gwas \
-$path_heritability \
-$path_genetic_correlation \
-$path_script_gwas_organization \
-$path_promiscuity_scripts \
-$report
+if false; then
+  # Yengo et al, Human Molecular Genetics, 2018 (PubMed:30124842)
+  # phenotype: body mass index
+  study="30124842_yengo_2018"
+  source_file="Meta-analysis_Locke_et_al+UKBiobank_2018_UPDATED.txt.gz"
+  path_source_directory="${path_gwas_summaries}/${study}"
+  path_source_file="${path_source_directory}/${source_file}"
+  path_script_gwas_organization="${path_scripts_organization}/organize_gwas_ldsc_${study}.sh"
+  report="true" # "true" or "false"
+  /usr/bin/bash "$path_scripts_record/3_organize_gwas_phenotype_ldsc_heritability.sh" \
+  $study \
+  $source_file \
+  $path_source_file \
+  $path_genetic_reference \
+  $path_gwas \
+  $path_heritability \
+  $path_genetic_correlation \
+  $path_script_gwas_organization \
+  $path_promiscuity_scripts \
+  $report
+fi
 
-# Pulit et al, Human Molecular Genetics, 2018 (PubMed:30239722)
-# phenotype: waist-to-hip ratio, adjusted for BMI
-study="30239722_pulit_2018"
-source_file="whradjbmi.giant-ukbb.meta-analysis.combined.23May2018.txt.gz"
-path_source_directory="${path_gwas_summaries}/${study}"
-path_source_file="${path_source_directory}/${source_file}"
-path_script_gwas_organization="${path_scripts_organization}/organize_gwas_ldsc_${study}.sh"
-report="true" # "true" or "false"
-/usr/bin/bash "$path_scripts_record/3_organize_gwas_phenotype_ldsc_heritability.sh" \
-$study \
-$source_file \
-$path_source_file \
-$path_genetic_reference \
-$path_gwas \
-$path_heritability \
-$path_genetic_correlation \
-$path_script_gwas_organization \
-$path_promiscuity_scripts \
-$report
+if true; then
+  # Pulit et al, Human Molecular Genetics, 2018 (PubMed:30239722)
+  # phenotype: waist-to-hip ratio, adjusted for BMI
+  study="30239722_pulit_2018"
+  source_file="whradjbmi.giant-ukbb.meta-analysis.combined.23May2018.txt.gz"
+  path_source_directory="${path_gwas_summaries}/${study}"
+  path_source_file="${path_source_directory}/${source_file}"
+  path_script_gwas_organization="${path_scripts_organization}/organize_gwas_ldsc_${study}.sh"
+  report="true" # "true" or "false"
+  /usr/bin/bash "$path_scripts_record/3_organize_gwas_phenotype_ldsc_heritability.sh" \
+  $study \
+  $source_file \
+  $path_source_file \
+  $path_genetic_reference \
+  $path_gwas \
+  $path_heritability \
+  $path_genetic_correlation \
+  $path_script_gwas_organization \
+  $path_promiscuity_scripts \
+  $report
+fi
