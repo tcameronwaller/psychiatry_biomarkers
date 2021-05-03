cabd7a2a4bba0f159fdd9eea4051c38f5463bfcf
T. Cameron Waller <tcameronwaller@gmail.com>
2021-05-03 17:16:41 -0500

    correct paths for access genetic reference

--- scripts/record/2021-05-04/1_access_reference_genetic_correlation.sh
+++ scripts/record/2021-05-04/1_access_reference_genetic_correlation.sh
@@ -5,16 +5,17 @@
 # Read private, local file paths.
 #echo "read private file path variables and organize paths..."
 cd ~/paths
-path_temporary=$(<"./processing_bipolar_metabolism.txt")
-path_waller="$path_temporary/waller"
-path_dock="$path_temporary/waller/dock"
-path_genetic_reference="$path_dock/access/genetic_reference"
-path_promiscuity_scripts="$path_waller/promiscuity/scripts"
-path_promiscuity_scripts_ldsc="$path_promiscuity_scripts/ldsc_genetic_heritability_correlation"
+path_process=$(<"./process_psychiatric_metabolism.txt")
+
+path_dock="${path_process}/dock"
+path_genetic_reference="${path_dock}/access/genetic_reference"
+
+path_promiscuity_scripts="${path_process}/promiscuity/scripts"
+path_promiscuity_scripts_ldsc="${path_promiscuity_scripts}/ldsc_genetic_heritability_correlation"
 
 ###########################################################################
 # Execute procedure.
 ###########################################################################
 
-/usr/bin/bash "$path_promiscuity_scripts_ldsc/access_ldsc_genetic_references.sh" \
+/usr/bin/bash "${path_promiscuity_scripts_ldsc}/access_ldsc_genetic_references.sh" \
 $path_genetic_reference
