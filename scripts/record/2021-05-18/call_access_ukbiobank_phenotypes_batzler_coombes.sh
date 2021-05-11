ca09c5789e692c1ab9b773183979194096df62d9
T. Cameron Waller <tcameronwaller@gmail.com>
2021-05-11 13:16:56 -0500

    trial of team variable accession and definitions

---
+++ scripts/record/2021-05-18/call_access_ukbiobank_phenotypes_batzler_coombes.sh
@@ -0,0 +1,56 @@
+#!/bin/bash
+
+#chmod u+x script.sh
+
+###########################################################################
+###########################################################################
+###########################################################################
+# This script organizes paths and parameters to access local information
+# about persons and phenotypes in the UKBiobank.
+###########################################################################
+###########################################################################
+###########################################################################
+
+# version: 1
+
+###########################################################################
+# Organize script parameters.
+
+###########################################################################
+# Read private, local file paths.
+echo "read private file path variables and organize paths..."
+cd ~/paths
+path_scripts_team_uk_biobank=$(<"./ukbiobank_scripts.txt")
+path_process=$(<"./process_psychiatric_metabolism.txt")
+
+path_scripts_uk_biobank="$path_process/uk_biobank/scripts"
+path_script_batzler_access="$path_scripts_team_uk_biobank/test.sh"
+
+path_dock="${path_process}/dock"
+path_import="${path_dock}/access/ukbiobank_import"
+path_variables="${path_import}/variables.txt"
+
+# Echo each command to console.
+#set -x
+# Suppress echo each command to console.
+set +x
+
+# Determine whether the temporary directory structure already exists.
+if [ ! -d $path_access ]; then
+    # Directory does not already exist.
+    # Create directory.
+    mkdir -p $path_dock
+    mkdir -p $path_access
+fi
+
+# Specify UKBiobank phenotype variables of interest.
+# Only use this method if not using the file in the parameter directory.
+#echo "specify phenotype variables to access from UKBiobank"
+echo "22009 22006 31 22001 21022 21002 50 21001 23104 30890" | tr -s " " "\n" > $path_variables
+
+# Access phenotype variables and auxiliary information from UKBiobank.
+/usr/bin/bash "$path_script_batzler_access" \
+-n "waller_access" \
+-f $path_variables \
+-o $path_import \
+-d "defineUKB_variables.R" \
