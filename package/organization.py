9f7f9181f7992c8d96d2400cdfce3725a5fef662
T. Cameron Waller <tcameronwaller@gmail.com>
2021-05-25 08:59:17 -0500

    write tables for cohorts models

--- package/organization.py
+++ package/organization.py
@@ -282,7 +282,93 @@ def select_organize_metabolites_valid_identities_scores(
 # Write
 
 
+def write_product_cohort_table(
+    name=None,
+    information=None,
+    path_parent=None,
+):
+    """
+    Writes product information to file.
+
+    arguments:
+        name (str): base name for file
+        information (object): information to write to file
+        path_parent (str): path to parent directory
+
+    raises:
+
+    returns:
+
+    """
+
+    # Specify directories and files.
+    path_table = os.path.join(
+        path_parent, str(name + ".tsv")
+    )
+    # Write information to file.
+    information.to_csv(
+        path_or_buf=path_table,
+        sep="\t",
+        header=True,
+        index=False,
+    )
+    pass
+
+
+def write_product_cohorts(
+    information=None,
+    path_parent=None,
+):
+    """
+    Writes product information to file.
+
+    arguments:
+        information (object): information to write to file
+        path_parent (str): path to parent directory
+
+    raises:
+
+    returns:
+
+    """
+
+    for name in information.keys():
+        write_product_cohort_table(
+            name=name,
+            information=information[name],
+            path_parent=path_parent,
+        )
+    pass
+
+
 def write_product(
+    information=None,
+    paths=None,
+):
+    """
+    Writes product information to file.
+
+    arguments:
+        information (object): information to write to file
+        paths (dict<str>): collection of paths to directories for procedure's
+            files
+
+    raises:
+
+    returns:
+
+    """
+
+    # Cohort tables in PLINK format.
+    if True:
+        write_product_cohorts(
+            information=information["cohorts"],
+            path_parent=paths["cohorts"],
+        )
+    pass
+
+
+def write_product_metabolites_former(
     information=None,
     paths=None,
 ):
@@ -344,9 +430,6 @@ def write_product(
 ###############################################################################
 # Procedure
 
-# TODO: 1. organize BMI and log BMI
-# TODO: 2. organize tables for cohorts and covariates...
-
 
 def execute_procedure(
     path_dock=None,
@@ -392,13 +475,7 @@ def execute_procedure(
         table=pail_basis["table_clean"],
         report=True,
     )
-
-    print(pail_psychology["table_clean"].columns.to_list())
-
-
-    ###############
-    # TODO: maybe a new function to organize cohorts and covariates by case / control status...
-    # TODO: include a flag argument for "white_british"
+    #print(pail_psychology["table_clean"].columns.to_list())
 
     # Select and organize variables across cohorts.
     # Organize phenotypes and covariates in format for analysis in PLINK.
@@ -408,15 +485,14 @@ def execute_procedure(
             report=True,
     ))
 
-    if False:
-        # Collect information.
-        information = dict()
-        information["table_phenotypes"] = table_mental
-        # Write product information to file.
-        write_product(
-            paths=paths,
-            information=information
-        )
+    # Collect information.
+    information = dict()
+    information["cohorts"] = pail_cohorts
+    # Write product information to file.
+    write_product(
+        paths=paths,
+        information=information
+    )
     pass
 
 
