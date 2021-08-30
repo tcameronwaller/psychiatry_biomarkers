be4ad958c3c1f43e3f834f8ab181cac40dedcabf
T. Cameron Waller <tcameronwaller@gmail.com>
2021-08-30 16:45:47 -0500

    begin new GWAS pipe record, new UK Biobank accession variables

--- scripts/execute_psychiatric_metabolism.sh
+++ scripts/execute_psychiatric_metabolism.sh
@@ -36,19 +36,6 @@ fi
 # Collect and aggregate genetic scores for metabolites across the UK Biobank.
 #python3 $path_package/interface.py main --path_dock $path_dock --aggregation
 
-# Assemble phenotypic information from UK Biobank.
-#python3 $path_package/interface.py main --path_dock $path_dock --assembly
-
-# Organize phenotypic information from UK Biobank.
-python3 $path_package/interface.py main --path_dock $path_dock --organization
-
-# Regress phenotypes against metabolites' genetic scores across the UK Biobank.
-#python3 $path_package/interface.py main --path_dock $path_dock --association
-
-
-# TODO: new module...
-# TODO: start with function for 1 metabolite...
-# TODO: combine metabolite genetic score(s) with UK Biobank phenotypes
-# TODO: adapt regression code from "bimodality"...
-
-# TODO: organize UK Biobank phenotypes for Bipolar Disorder, Depression, etc
+python3 $path_package/interface.py main --path_dock $path_dock --assembly
+#python3 $path_package/interface.py main --path_dock $path_dock --merge_import
+#python3 $path_package/interface.py main --path_dock $path_dock --organization
