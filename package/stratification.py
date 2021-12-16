b6211af5e3a2dbfbebfad6688f3daa7b1a7a37b6
T. Cameron Waller <tcameronwaller@gmail.com>
2021-12-15 23:20:55 -0600

    organize stratification

--- package/stratification.py
+++ package/stratification.py
@@ -40,65 +40,6 @@ import uk_biobank.stratification as ukb_strat
 # Functionality
 
 
-
-##########
-# Initialization
-
-
-def initialize_directories(
-    restore=None,
-    path_dock=None,
-):
-    """
-    Initialize directories for procedure's product files.
-
-    arguments:
-        restore (bool): whether to remove previous versions of data
-        path_dock (str): path to dock directory for source and product
-            directories and files
-
-    raises:
-
-    returns:
-        (dict<str>): collection of paths to directories for procedure's files
-
-    """
-
-    # Collect paths.
-    paths = dict()
-    # Define paths to directories.
-    paths["dock"] = path_dock
-    paths["stratification"] = os.path.join(path_dock, "stratification")
-    paths["reference_population"] = os.path.join(
-        path_dock, "stratification", "reference_population"
-    )
-    paths["cohorts_models_linear"] = os.path.join(
-        path_dock, "stratification", "cohorts_models_linear"
-    )
-    paths["cohorts_models_logistic"] = os.path.join(
-        path_dock, "stratification", "cohorts_models_logistic"
-    )
-
-    # Remove previous files to avoid version or batch confusion.
-    if restore:
-        utility.remove_directory(path=paths["stratification"])
-    # Initialize directories.
-    utility.create_directories(
-        path=paths["stratification"]
-    )
-    utility.create_directories(
-        path=paths["reference_population"]
-    )
-    utility.create_directories(
-        path=paths["cohorts_models_linear"]
-    )
-    utility.create_directories(
-        path=paths["cohorts_models_logistic"]
-    )
-    # Return information.
-    return paths
-
-
 ##########
 # Read
 
@@ -169,7 +110,7 @@ def execute_procedure(
     time.sleep(5.0)
 
     # Initialize directories.
-    paths = initialize_directories(
+    paths = ukb_strat.initialize_directories(
         restore=True,
         path_dock=path_dock,
     )
@@ -182,6 +123,7 @@ def execute_procedure(
 
     # Select and organize variables across cohorts.
     # Organize phenotypes and covariates in format for analysis in PLINK.
+
     # Reference population.
     if True:
         pail_population = (
@@ -191,49 +133,37 @@ def execute_procedure(
                 path_dock=path_dock,
                 report=True,
         ))
-        # Describe effect of preferential selection of females and males in the
-        # Kinship Filter for genetic analyses.
-        # Priority for female persons.
-        table_simple = (
-            pail_population["table_white_unrelated_female_male"]
-        )
-        table_priority = (
-            pail_population["table_white_unrelated_female_male_priority_female"]
-        )
-        ukb_strat.report_kinship_filter_priority_selection(
-            name="... Comparison of priority to female persons in population ...",
-            priority_values=["female",],
-            priority_variable="sex_text",
-            table_full=source["table_phenotypes"],
-            table_simple=table_simple,
-            table_priority=table_priority,
-            report=True,
-        )
-        # Priority for male persons.
-        table_simple = (
-            pail_population["table_white_unrelated_female_male"]
-        )
-        table_priority = (
-            pail_population["table_white_unrelated_female_male_priority_male"]
-        )
-        ukb_strat.report_kinship_filter_priority_selection(
-            name="... Comparison of priority to male persons in population ...",
-            priority_values=["male",],
-            priority_variable="sex_text",
-            table_full=source["table_phenotypes"],
-            table_simple=table_simple,
-            table_priority=table_priority,
-            report=True,
-        )
     else:
         pail_population = dict()
         pass
-    # Cohorts and models for linear genetic analyses.
 
-    # set: Vitamin D...
-
+    # Hormones and their regulatory proteins.
     if False:
-        pail_linear = (
+        pail_hormones_linear = (
+            ukb_strat.execute_stratify_genotype_cohorts_plink_format_set(
+                table=source["table_phenotypes"],
+                set="hormones_linear",
+                path_dock=path_dock,
+                report=True,
+        ))
+    else:
+        pail_hormones_linear = dict()
+        pass
+    if False:
+        pail_hormones_logistic = (
+            ukb_strat.execute_stratify_genotype_cohorts_plink_format_set(
+                table=source["table_phenotypes"],
+                set="hormones_logistic",
+                path_dock=path_dock,
+                report=True,
+        ))
+    else:
+        pail_hormones_logistic = dict()
+        pass
+
+    # Body mass index (BMI) in Bipolar Disorder.
+    if True:
+        pail_bipolar_linear = (
             ukb_strat.execute_stratify_genotype_cohorts_plink_format_set(
                 table=source["table_phenotypes"],
                 set="bipolar_body_linear",
@@ -241,60 +171,27 @@ def execute_procedure(
                 report=True,
         ))
     else:
-        pail_linear = dict()
+        pail_bipolar_linear = dict()
         pass
-    # Cohorts and models for logistic genetic analyses.
-    if False:
-        pail_logistic = (
+    if True:
+        pail_bipolar_logistic = (
             ukb_strat.execute_stratify_genotype_cohorts_plink_format_set(
                 table=source["table_phenotypes"],
                 set="bipolar_body_logistic",
                 path_dock=path_dock,
                 report=True,
         ))
-        # Describe effect of preferential selection of Bipolar Disorder cases in the
-        # Kinship Filter for genetic analyses.
-        # Strict Bipolar Disorder.
-        table_simple = (
-            pail_logistic["table_white_bipolar_control_case_strict"]
-        )
-        table_priority = (
-            pail_logistic["table_white_bipolar_control_case_strict_priority_case"]
-        )
-        ukb_strat.report_kinship_filter_priority_selection(
-            name="... Comparison of case priority for Strict Bipolar Disorder ...",
-            priority_values=[1,],
-            priority_variable="bipolar_control_case_strict",
-            table_full=source["table_phenotypes"],
-            table_simple=table_simple,
-            table_priority=table_priority,
-            report=True,
-        )
-        # Loose Bipolar Disorder.
-        table_simple = (
-            pail_logistic["table_white_bipolar_control_case_loose"]
-        )
-        table_priority = (
-            pail_logistic["table_white_bipolar_control_case_loose_priority_case"]
-        )
-        ukb_strat.report_kinship_filter_priority_selection(
-            name="... Comparison of case priority for Loose Bipolar Disorder ...",
-            priority_values=[1,],
-            priority_variable="bipolar_control_case_loose",
-            table_full=source["table_phenotypes"],
-            table_simple=table_simple,
-            table_priority=table_priority,
-            report=True,
-        )
     else:
-        pail_logistic = dict()
+        pail_bipolar_logistic = dict()
         pass
 
     # Collect information.
     information = dict()
     information["reference_population"] = pail_population
-    information["cohorts_models_linear"] = pail_linear
-    information["cohorts_models_logistic"] = pail_logistic
+    information["hormones_linear"] = pail_hormones_linear
+    information["hormones_logistic"] = pail_hormones_logistic
+    information["body_bipolar_linear"] = pail_bipolar_linear
+    information["body_bipolar_logistic"] = pail_bipolar_logistic
     # Write product information to file.
     ukb_strat.write_genotype_product(
         paths=paths,
