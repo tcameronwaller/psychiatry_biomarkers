823c5f789f341e8021da6cf84581a0a9f2d90eaa
T. Cameron Waller <tcameronwaller@gmail.com>
2022-01-14 16:01:03 -0600

    execute organization of hormones before female-specific variables

--- package/organization.py
+++ package/organization.py
@@ -214,20 +214,20 @@ def execute_procedure(
         path_dock=path_dock,
         report=True,
     )
-    # Organize variables for female menstruation across the UK Biobank.
-    pail_female = ukb_organization.execute_female_menstruation(
-        table=pail_basis["table"],
-        report=True,
-    )
     # Organize variables for persons' sex hormones across the UK Biobank.
     pail_hormone = ukb_organization.execute_sex_hormones(
-        table=pail_female["table"],
+        table=pail_basis["table"],
         path_dock=path_dock,
+        report=True,
+    )
+    # Organize variables for female menstruation across the UK Biobank.
+    pail_female = ukb_organization.execute_female_menstruation(
+        table=pail_hormone["table"],
         report=True,
     )
     # Organize variables for persons' alcohol consumption across the UK Biobank.
     pail_alcohol = ukb_organization.execute_alcohol(
-        table=pail_hormone["table"],
+        table=pail_female["table"],
         report=True,
     )
     # Organize variables for persons' mental health across the UK Biobank.
