c9a746e3e55dedeb2b933bea6164bf49f0b53b2c
T. Cameron Waller <tcameronwaller@gmail.com>
2022-01-19 20:13:26 -0600

    simplify organization procedure to test female variables

--- package/organization.py
+++ package/organization.py
@@ -214,37 +214,44 @@ def execute_procedure(
         path_dock=path_dock,
         report=True,
     )
-    # Organize variables for persons' sex hormones across the UK Biobank.
-    pail_hormone = ukb_organization.execute_sex_hormones(
-        table=pail_basis["table"],
-        path_dock=path_dock,
-        report=True,
-    )
-    # Organize variables for female menstruation across the UK Biobank.
-    pail_female = ukb_organization.execute_female_menstruation(
-        table=pail_hormone["table"],
-        report=True,
-    )
-    # Organize variables for persons' alcohol consumption across the UK Biobank.
-    pail_alcohol = ukb_organization.execute_alcohol(
-        table=pail_female["table"],
-        report=True,
-    )
-    # Organize variables for persons' mental health across the UK Biobank.
-    pail_psychology = ukb_organization.execute_psychology_psychiatry(
-        table=pail_alcohol["table"],
-        path_dock=path_dock,
-        report=True,
-    )
-    #print(pail_psychology["table_clean"].columns.to_list())
+    if False:
+        # Organize variables for persons' sex hormones across the UK Biobank.
+        pail_hormone = ukb_organization.execute_sex_hormones(
+            table=pail_basis["table"],
+            path_dock=path_dock,
+            report=True,
+        )
+        # Organize variables for female menstruation across the UK Biobank.
+        pail_female = ukb_organization.execute_female_menstruation(
+            table=pail_hormone["table"],
+            report=True,
+        )
+        # Organize variables for persons' alcohol consumption across the UK Biobank.
+        pail_alcohol = ukb_organization.execute_alcohol(
+            table=pail_female["table"],
+            report=True,
+        )
+        # Organize variables for persons' mental health across the UK Biobank.
+        pail_psychology = ukb_organization.execute_psychology_psychiatry(
+            table=pail_alcohol["table"],
+            path_dock=path_dock,
+            report=True,
+        )
+        #print(pail_psychology["table_clean"].columns.to_list())
+    if True:
+        # Organize variables for female menstruation across the UK Biobank.
+        pail_female = ukb_organization.execute_female_menstruation(
+            table=pail_basis["table"],
+            report=True,
+        )
 
     # Collect information.
     information = dict()
     information["organization"] = dict()
     #information["organization"]["table_phenotypes"] = pail_basis["table"]
     #information["organization"]["table_phenotypes"] = pail_hormone["table"]
-    #information["organization"]["table_phenotypes"] = pail_female["table"]
-    information["organization"]["table_phenotypes"] = pail_psychology["table"]
+    information["organization"]["table_phenotypes"] = pail_female["table"]
+    #information["organization"]["table_phenotypes"] = pail_psychology["table"]
     # Write product information to file.
     ukb_organization.write_product(
         paths=paths,
