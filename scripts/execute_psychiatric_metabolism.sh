caeea2556a69d21e65819e6c4dd804daa5228afb
T. Cameron Waller <tcameronwaller@gmail.com>
2022-01-25 13:47:10 -0600

    verison check

--- scripts/execute_psychiatric_metabolism.sh
+++ scripts/execute_psychiatric_metabolism.sh
@@ -39,7 +39,7 @@ fi
 
 python3 $path_package/interface.py main --path_dock $path_dock --assembly # TCW 25 January 2022
 #python3 $path_package/interface.py main --path_dock $path_dock --importation # TCW 25 January 2022
-#python3 $path_package/interface.py main --path_dock $path_dock --organization # TCW 20 January 2022
+#python3 $path_package/interface.py main --path_dock $path_dock --organization # TCW 25 January 2022
 #python3 $path_package/interface.py main --path_dock $path_dock --stratification # TCW 4 January 2022
 
 #python3 $path_package/interface.py main --path_dock $path_dock --description
