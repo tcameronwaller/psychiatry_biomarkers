e0eefbdcd16db8d9719600f25189b326b13eea04
T. Cameron Waller <tcameronwaller@gmail.com>
2022-02-02 16:02:11 -0600

    organization procedure

--- scripts/execute_psychiatric_metabolism.sh
+++ scripts/execute_psychiatric_metabolism.sh
@@ -39,11 +39,11 @@ fi
 
 #python3 $path_package/interface.py main --path_dock $path_dock --assembly # TCW 25 January 2022
 #python3 $path_package/interface.py main --path_dock $path_dock --importation # TCW 25 January 2022
-#python3 $path_package/interface.py main --path_dock $path_dock --organization # TCW 02 February 2022
+python3 $path_package/interface.py main --path_dock $path_dock --organization # TCW 02 February 2022
 #python3 $path_package/interface.py main --path_dock $path_dock --stratification # TCW 31 January 2022
 
 #python3 $path_package/interface.py main --path_dock $path_dock --description
-python3 $path_package/interface.py main --path_dock $path_dock --regression
+#python3 $path_package/interface.py main --path_dock $path_dock --regression
 #python3 $path_package/interface.py main --path_dock $path_dock --collection
 
 #python3 $path_package/interface.py main --path_dock $path_dock --scratch
