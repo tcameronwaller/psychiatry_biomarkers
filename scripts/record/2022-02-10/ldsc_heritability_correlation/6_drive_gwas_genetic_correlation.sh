d1e0ef593528d2f9be523f01bfce0daf8dcdfe73
T. Cameron Waller <tcameronwaller@gmail.com>
2022-02-07 12:04:33 -0600

    read for genetic correlation comparisons between studies

--- scripts/record/2022-02-10/ldsc_heritability_correlation/6_drive_gwas_genetic_correlation.sh
+++ scripts/record/2022-02-10/ldsc_heritability_correlation/6_drive_gwas_genetic_correlation.sh
@@ -11,13 +11,9 @@
 ################################################################################
 # General parameters.
 
-cohorts_models="cohorts_models_linear_measurement" # TCW started at 12:16 on 9 December 2021
-#cohorts_models="cohorts_models_linear_measurement_unadjust"
-#cohorts_models="cohorts_models_linear_imputation"
-#cohorts_models="cohorts_models_linear_imputation_unadjust"
-
-#cohorts_models="cohorts_models_logistic_detection"
-#cohorts_models="cohorts_models_logistic_detection_unadjust"
+cohorts_models="white_female_male_priority_male_linear" # 8 GWAS; TCW ran at ____ on 03 February 2022
+#cohorts_models="white_female_linear"                    # 8 GWAS; TCW ran at ____ on 03 February 2022
+#cohorts_models="white_male_linear"                      # 8 GWAS; TCW ran at ____ on 03 February 2022
 
 ################################################################################
 # Organize paths.
@@ -34,9 +30,6 @@ path_secondary_gwas_munge_container="${path_dock}/gwas_ldsc_munge/${cohorts_mode
 ###########################################################################
 # Define main comparisons.
 
-# TODO: TCW 9 December 2021
-# TODO: need to update primaries
-
 # Define array of primary studies.
 primaries=()
 primaries+=("30482948_walters_2018_eur_unrel_meta;${path_primary_gwas_munge_container}/30482948_walters_2018_eur_unrel_meta/${name_gwas_munge_file}")
@@ -45,11 +38,11 @@ primaries+=("30482948_walters_2018_female;${path_primary_gwas_munge_container}/3
 primaries+=("30482948_walters_2018_male;${path_primary_gwas_munge_container}/30482948_walters_2018_male/${name_gwas_munge_file}")
 primaries+=("30643251_liu_2019_alcohol_all;${path_primary_gwas_munge_container}/30643251_liu_2019_alcohol_all/${name_gwas_munge_file}")
 primaries+=("30643251_liu_2019_alcohol_no_ukb;${path_primary_gwas_munge_container}/30643251_liu_2019_alcohol_no_ukb/${name_gwas_munge_file}")
-primaries+=("30336701_sanchez-roige_2018_audit;${path_primary_gwas_munge_container}/30336701_sanchez-roige_2018_audit/${name_gwas_munge_file}")
 primaries+=("30718901_howard_2019;${path_primary_gwas_munge_container}/30718901_howard_2019/${name_gwas_munge_file}")
 primaries+=("34002096_mullins_2021_all;${path_primary_gwas_munge_container}/34002096_mullins_2021_all/${name_gwas_munge_file}")
 primaries+=("34002096_mullins_2021_bpd1;${path_primary_gwas_munge_container}/34002096_mullins_2021_bpd1/${name_gwas_munge_file}")
 primaries+=("34002096_mullins_2021_bpd2;${path_primary_gwas_munge_container}/34002096_mullins_2021_bpd2/${name_gwas_munge_file}")
+primaries+=("00000000_ripke_2022;${path_primary_gwas_munge_container}/00000000_ripke_2022/${name_gwas_munge_file}")
 
 # Define array of secondary studies.
 secondaries=()
@@ -83,61 +76,44 @@ done
 # Append custom comparisons that do not follow the same pattern.
 
 ##########
-# cohorts_models_linear_measurement
+# Study pairs within the same container (white_female_male_priority_male_linear).
 if true; then
-  # Females to Males.
-  pairs+=("female_vitamin_d_log;male_vitamin_d_log")
-  pairs+=("female_albumin;male_albumin")
-  pairs+=("female_steroid_globulin_log;male_steroid_globulin_log")
-  pairs+=("female_oestradiol_log;male_oestradiol_log")
-  pairs+=("female_premenopause_oestradiol_log;male_oestradiol_log")
-  pairs+=("female_perimenopause_oestradiol_log;male_oestradiol_log")
-  pairs+=("female_postmenopause_oestradiol_log;male_oestradiol_log")
-  pairs+=("female_testosterone_log;male_testosterone_log")
-  # Premenopause Females to Postmenopause Females.
-  pairs+=("female_premenopause_vitamin_d_log;female_postmenopause_vitamin_d_log")
-  pairs+=("female_premenopause_albumin;female_postmenopause_albumin")
-  pairs+=("female_premenopause_steroid_globulin_log;female_postmenopause_steroid_globulin_log")
-  pairs+=("female_premenopause_oestradiol_log;female_postmenopause_oestradiol_log")
-  pairs+=("female_premenopause_testosterone_log;female_postmenopause_testosterone_log")
-  # Younger Males to Older Males.
-  pairs+=("male_age_low_vitamin_d_log;male_age_high_vitamin_d_log")
-  pairs+=("male_age_low_albumin;male_age_high_albumin")
-  pairs+=("male_age_low_steroid_globulin_log;male_age_high_steroid_globulin_log")
-  pairs+=("male_age_low_oestradiol_log;male_age_high_oestradiol_log")
-  pairs+=("male_age_low_testosterone_log;male_age_high_testosterone_log")
-fi
-
-##########
-# cohorts_models_linear_imputation
-if false; then
-  # Females to Males.
-  pairs+=("female_vitamin_d_imputation_log;male_vitamin_d_imputation_log")
-  pairs+=("female_albumin_imputation;male_albumin_imputation")
-  pairs+=("female_steroid_globulin_imputation_log;male_steroid_globulin_imputation_log")
-  pairs+=("female_oestradiol_imputation;male_oestradiol_imputation")
-  pairs+=("female_testosterone_imputation;male_testosterone_imputation")
-  # Premenopause Females to Postmenopause Females.
-  pairs+=("female_premenopause_vitamin_d_imputation_log;female_postmenopause_vitamin_d_imputation_log")
-  pairs+=("female_premenopause_albumin_imputation;female_postmenopause_albumin_imputation")
-  pairs+=("female_premenopause_steroid_globulin_imputation_log;female_postmenopause_steroid_globulin_imputation_log")
-  pairs+=("female_premenopause_oestradiol_imputation;female_postmenopause_oestradiol_imputation")
-  pairs+=("female_premenopause_testosterone_imputation;female_postmenopause_testosterone_imputation")
-  # Younger Males to Older Males.
-  pairs+=("male_age_low_vitamin_d_imputation_log;male_age_high_vitamin_d_imputation_log")
-  pairs+=("male_age_low_albumin_imputation;male_age_high_albumin_imputation")
-  pairs+=("male_age_low_steroid_globulin_imputation_log;male_age_high_steroid_globulin_imputation_log")
-  pairs+=("male_age_low_oestradiol_imputation;male_age_high_oestradiol_imputation")
-  pairs+=("male_age_low_testosterone_imputation;male_age_high_testosterone_imputation")
+  # Signal transformation.
+  pairs+=("joint_vitamin_d;joint_vitamin_d_log")
+  pairs+=("joint_vitamin_d_imputation;joint_vitamin_d_imputation_log")
+  # Model.
+  pairs+=("unadjust_vitamin_d_imputation_log;joint_vitamin_d_imputation_log")
 fi
 
 # Assemble array of batch instance details.
-comparison_container="${cohorts_models}_secondary_pairs"
+comparison_container="${cohorts_models}_secondary_pairs_model"
 for pair in "${pairs[@]}"; do
   IFS=";" read -r -a array <<< "${pair}"
   study_primary="${array[0]}"
   study_secondary="${array[1]}"
   comparisons+=("${comparison_container};${study_primary};${path_secondary_gwas_munge_container}/${study_primary}/${name_gwas_munge_file};${study_secondary};${path_secondary_gwas_munge_container}/${study_secondary}/${name_gwas_munge_file}")
+done
+
+
+##########
+# Study pairs within different containers.
+if true; then
+  # Females to Males.
+  pairs+=("female_vitamin_d;${path_dock}/gwas_ldsc_munge/white_female_linear/joint_vitamin_d;male_vitamin_d;${path_dock}/gwas_ldsc_munge/white_male_linear/joint_vitamin_d")
+  pairs+=("female_vitamin_d_log;${path_dock}/gwas_ldsc_munge/white_female_linear/joint_vitamin_d_log;male_vitamin_d_log;${path_dock}/gwas_ldsc_munge/white_male_linear/joint_vitamin_d_log")
+  pairs+=("female_vitamin_d_imputation;${path_dock}/gwas_ldsc_munge/white_female_linear/joint_vitamin_d_imputation;male_vitamin_d_imputation;${path_dock}/gwas_ldsc_munge/white_male_linear/joint_vitamin_d_imputation")
+  pairs+=("female_vitamin_d_imputation_log;${path_dock}/gwas_ldsc_munge/white_female_linear/joint_vitamin_d_imputation_log;male_vitamin_d_imputation_log;${path_dock}/gwas_ldsc_munge/white_male_linear/joint_vitamin_d_imputation_log")
+fi
+
+# Assemble array of batch instance details.
+comparison_container="${cohorts_models}_secondary_pairs_female_male"
+for pair in "${pairs[@]}"; do
+  IFS=";" read -r -a array <<< "${pair}"
+  study_primary="${array[0]}"
+  path_primary="${array[1]}"
+  study_secondary="${array[2]}"
+  path_secondary="${array[3]}"
+  comparisons+=("${comparison_container};${study_primary};${path_primary}/${name_gwas_munge_file};${study_secondary};${path_secondary}/${name_gwas_munge_file}")
 done
 
 ################################################################################
