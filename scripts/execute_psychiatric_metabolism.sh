c7f1739905ab7c30aeef7b021565a53605cba9da
T. Cameron Waller <tcameronwaller@gmail.com>
2022-02-10 08:28:17 -0600

    regression procedure

--- scripts/execute_psychiatric_metabolism.sh
+++ scripts/execute_psychiatric_metabolism.sh
@@ -39,11 +39,11 @@ fi
 
 #python3 $path_package/interface.py main --path_dock $path_dock --assembly # TCW 09 February 2022
 #python3 $path_package/interface.py main --path_dock $path_dock --importation # TCW 09 February 2022
-python3 $path_package/interface.py main --path_dock $path_dock --organization # TCW __ February 2022
+#python3 $path_package/interface.py main --path_dock $path_dock --organization # TCW 09 February 2022
 #python3 $path_package/interface.py main --path_dock $path_dock --stratification # TCW 31 January 2022
 
 #python3 $path_package/interface.py main --path_dock $path_dock --description
-#python3 $path_package/interface.py main --path_dock $path_dock --regression # TCW 02 February 2022
+python3 $path_package/interface.py main --path_dock $path_dock --regression # TCW __ February 2022
 #python3 $path_package/interface.py main --path_dock $path_dock --collection
 
 #python3 $path_package/interface.py main --path_dock $path_dock --scratch
