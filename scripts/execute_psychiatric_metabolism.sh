51311a24f868913f2e972378d86b0b0f6fdb373a
T. Cameron Waller <tcameronwaller@gmail.com>
2022-02-22 11:58:03 -0600

    assembly procedure

--- scripts/execute_psychiatric_metabolism.sh
+++ scripts/execute_psychiatric_metabolism.sh
@@ -37,9 +37,9 @@ fi
 
 # Execute procedure(s).
 
-#python3 $path_package/interface.py main --path_dock $path_dock --assembly # TCW 21 February 2022
-#python3 $path_package/interface.py main --path_dock $path_dock --importation # TCW 22 February 2022
-python3 $path_package/interface.py main --path_dock $path_dock --organization # TCW __ February 2022
+python3 $path_package/interface.py main --path_dock $path_dock --assembly # TCW __ February 2022
+#python3 $path_package/interface.py main --path_dock $path_dock --importation # TCW __ February 2022
+#python3 $path_package/interface.py main --path_dock $path_dock --organization # TCW __ February 2022
 #python3 $path_package/interface.py main --path_dock $path_dock --stratification # TCW __ February 2022
 
 #python3 $path_package/interface.py main --path_dock $path_dock --description
