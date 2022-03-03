de9826e7edba8095a36781b40fa91d9893a30447
T. Cameron Waller <tcameronwaller@gmail.com>
2022-03-02 18:12:22 -0600

    activate procedure for oestradiol_bioavailable_linear

--- scripts/record/2022-03-07/gwas_concatenation/1-1_call_submit_gwas_concatenation.sh
+++ scripts/record/2022-03-07/gwas_concatenation/1-1_call_submit_gwas_concatenation.sh
@@ -11,10 +11,10 @@
 ################################################################################
 # General parameters.
 
-cohorts_models="vitamin_d_linear"                   # 4 GWAS; GWAS job 3155905, status: complete;
+#cohorts_models="vitamin_d_linear"                   # 4 GWAS; GWAS job 3155905, status: complete; TCW started at 18:11 on 02 March 2022;
 
 ###cohorts_models="oestradiol_logistic"              # 24 GWAS; GWAS job 3202343, status: running; <-- priority!!!
-#cohorts_models="oestradiol_bioavailable_linear"     # 18 GWAS; GWAS job 3149651, status: complete; TCW started at __ on 02 March 2022;
+cohorts_models="oestradiol_bioavailable_linear"     # 18 GWAS; GWAS job 3149651, status: complete; TCW started at __ on 02 March 2022;
 #cohorts_models="oestradiol_free_linear"             # 18 GWAS; GWAS job 3149652, status: complete; TCW started at __ on 02 March 2022;
 
 ###cohorts_models="testosterone_logistic"            # 24 GWAS; GWAS job 3202423, status: in queue;
