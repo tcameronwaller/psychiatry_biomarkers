1d2859727ef6692dd65865504227b5543bcf1a12
T. Cameron Waller <tcameronwaller@gmail.com>
2022-03-03 12:58:12 -0600

    concatenation batch for oestradiol_logistic

--- scripts/record/2022-03-07/gwas_concatenation/1-1_call_submit_gwas_concatenation.sh
+++ scripts/record/2022-03-07/gwas_concatenation/1-1_call_submit_gwas_concatenation.sh
@@ -13,7 +13,8 @@
 
 #cohorts_models="vitamin_d_linear"                   # 4 GWAS; GWAS job 3155905, status: complete; TCW started at 18:11 on 02 March 2022;
 
-###cohorts_models="oestradiol_logistic"              # 24 GWAS; GWAS job 3202343, status: running; <-- priority!!!
+cohorts_models="oestradiol_logistic"                # 12 GWAS; GWAS job 3202343, status: complete; TCW started at ____ on 03 March 2022;
+###cohorts_models="oestradiol_logistic_long"         # 12 GWAS; GWAS job 3225508, status: running;
 #cohorts_models="oestradiol_bioavailable_linear"     # 18 GWAS; GWAS job 3149651, status: complete; TCW started at 18:13 on 02 March 2022;
 #cohorts_models="oestradiol_free_linear"             # 18 GWAS; GWAS job 3149652, status: complete; TCW started at 18:15 on 02 March 2022;
 
@@ -50,8 +51,8 @@ mkdir -p $path_gwas_target_container
 rm $path_batch_instances
 
 # Define patterns for file names.
-pattern_gwas_report_file="report.*.glm.linear" # do not expand with full path yet <-- must choose here!!!
-#pattern_gwas_report_file="report.*.glm.logistic" # do not expand with full path yet <-- must choose here!!!
+#pattern_gwas_report_file="report.*.glm.linear" # do not expand with full path yet <-- must choose here!!!
+pattern_gwas_report_file="report.*.glm.logistic" # do not expand with full path yet <-- must choose here!!!
 pattern_gwas_concatenation_file="gwas_concatenation.txt.gz"
 
 # Iterate on directories for GWAS on cohorts and hormones.
