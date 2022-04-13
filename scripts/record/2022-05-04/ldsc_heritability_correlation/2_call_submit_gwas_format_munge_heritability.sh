f88d04f9148e5dbbc9e4970c1f8669a7a1e6c994
T. Cameron Waller <tcameronwaller@gmail.com>
2022-04-13 15:57:15 -0500

    ready to submit GWAS format munge batch 'testosterone_free_linear'

--- scripts/record/2022-05-04/ldsc_heritability_correlation/2_call_submit_gwas_format_munge_heritability.sh
+++ scripts/record/2022-05-04/ldsc_heritability_correlation/2_call_submit_gwas_format_munge_heritability.sh
@@ -23,8 +23,8 @@
 #cohorts_models="oestradiol_free_linear"                # 18 GWAS; 13 April 2022
 #cohorts_models="testosterone_logistic"                 # __ GWAS; incomplete
 #cohorts_models="testosterone_linear"                   # 54 GWAS; 13 April 2022
-cohorts_models="testosterone_bioavailable_linear"      # 18 GWAS; __ April 2022
-#cohorts_models="testosterone_free_linear"              # 18 GWAS; __ April 2022
+#cohorts_models="testosterone_bioavailable_linear"      # 18 GWAS; 13 April 2022
+cohorts_models="testosterone_free_linear"              # 18 GWAS; __ April 2022
 
 # Parameters.
 regression_type="linear" # "linear" or "logistic"
