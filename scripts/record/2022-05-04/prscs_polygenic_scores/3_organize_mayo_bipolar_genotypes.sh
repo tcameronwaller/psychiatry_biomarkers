cd6a1106e0241ed18d7d5c9ac7fc29272a0576a5
T. Cameron Waller <tcameronwaller@gmail.com>
2022-05-11 17:34:36 -0500

    script for information extraction and format conversion VCF to BIM

--- scripts/record/2022-05-04/prscs_polygenic_scores/3_organize_mayo_bipolar_genotypes.sh
+++ scripts/record/2022-05-04/prscs_polygenic_scores/3_organize_mayo_bipolar_genotypes.sh
@@ -16,14 +16,12 @@ path_promiscuity_scripts="${path_process}/promiscuity/scripts"
 path_script_drive_convert_vcf_to_bim="${path_promiscuity_scripts}/utility/drive_convert_directory_all_vcf_to_plink_bim.sh"
 
 ###########################################################################
-# Organize paths.
+# Organize variables.
 
 # Define patterns for file names.
 pattern_genotype_source_vcf_file="MERGED.maf0.dosR20.3.noDups.chr*.dose.vcf.gz" # do not expand with full path yet
 
-# Define prefix for names of
-name_prefix_file_product_bim=""
-
+name_prefix_file_product_bim="test_"
 report="true"
 
 ###########################################################################
@@ -34,13 +32,14 @@ report="true"
 set -x
 
 # Initialize directories and batch instances.
-#rm -r $path_genotype_product_bim_container
+rm -r $path_genotype_product_bim_container
 mkdir -p $path_genotype_product_bim_container
 
-# Convert genotype files in VCF format to BIM format.
+# Convert information from genotype files in VCF format to BIM format.
 /usr/bin/bash "${path_script_drive_convert_vcf_to_bim}" \
 $path_mayo_bipolar_genotype \
 $pattern_genotype_source_vcf_file \
 $path_genotype_product_bim_container \
 $name_prefix_file_product_bim \
+$path_promiscuity_scripts \
 $report
