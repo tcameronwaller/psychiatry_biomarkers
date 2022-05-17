cbd2041b2b08b61d8266dbf084872915a6cbcbee
T. Cameron Waller <tcameronwaller@gmail.com>
2022-05-17 14:35:20 -0500

    test script to remove chr prefix from VCF file

--- scripts/record/2022-05-04/prscs_polygenic_scores/4_organize_mayo_bipolar_genotypes.sh
+++ scripts/record/2022-05-04/prscs_polygenic_scores/4_organize_mayo_bipolar_genotypes.sh
@@ -7,10 +7,15 @@
 cd ~/paths
 path_bcftools=$(<"./tools_bcftools.txt")
 path_plink2=$(<"./tools_plink2.txt")
+
 path_process=$(<"./process_psychiatric_metabolism.txt")
+path_parameters="$path_process/dock/parameters"
+path_translations_chromosome_prefix="$path_parameters/promiscuity/translations_chromosomes_chr_prefix.txt"
 path_dock="${path_process}/dock"
 path_dbsnp_reference="${path_dock}/access/dbsnp_reference/GCF_000001405.39.gz"
 path_mayo_bipolar_genotype="${path_dock}/access/mayo_bipolar_genotype"
+
+path_genotype_chr_format="${path_dock}/access/mayo_bipolar_genotype/chr_format"
 path_genotype_dbsnp_annotation="${path_dock}/access/mayo_bipolar_genotype/dbsnp_annotation"
 path_genotype_snp_bim="${path_dock}/access/mayo_bipolar_genotype/snp_relevance_bim"
 
@@ -18,6 +23,7 @@ path_genotype_snp_bim="${path_dock}/access/mayo_bipolar_genotype/snp_relevance_b
 path_promiscuity_scripts="${path_process}/promiscuity/scripts"
 
 path_scripts_utility="${path_promiscuity_scripts}/utility"
+path_script_chr_prefix_in_vcf="${path_scripts_utility}/remove_chr_prefix_in_vcf.sh"
 path_script_dbsnp_rsid_to_vcf="${path_scripts_utility}/introduce_dbsnp_rsid_to_vcf.sh"
 path_script_drive_convert_vcf_to_bim="${path_scripts_utility}/drive_convert_directory_all_vcf_to_plink_bim.sh"
 
@@ -36,26 +42,46 @@ set -x
 # TODO: temporarily run test on a single genotype file
 # TODO: then write a "driver" script like I did for the VCF to BIM extraction
 
-
 ###########################################################################
-# Annotate information in VCF.
+# Format genotype information in VCF.
 
 # Initialize directory.
-#rm -r $path_genotype_dbsnp_annotation
-#mkdir -p $path_genotype_dbsnp_annotation
+rm -r $path_genotype_chr_format
+mkdir -p $path_genotype_chr_format
 
 path_vcf_source="${path_mayo_bipolar_genotype}/MERGED.maf0.dosR20.3.noDups.chr21.dose.vcf.gz"
-path_vcf_product="${path_genotype_dbsnp_annotation}/genotype_annotation_chromosome_21.vcf.gz"
-
+path_vcf_product="${path_genotype_chr_format}/genotype_chromosome_21.vcf.gz"
 report="true"
 
 # Convert information from genotype files in VCF format to BIM format.
-#/usr/bin/bash "${path_script_dbsnp_rsid_to_vcf}" \
-#$path_dbsnp_reference \
-#$path_vcf_source \
-#$path_vcf_product \
-#$path_bcftools \
-#$report
+/usr/bin/bash "${path_script_chr_prefix_in_vcf}" \
+$path_translations_chromosome_prefix \
+$path_vcf_source \
+$path_vcf_product \
+$path_bcftools \
+$report
+
+###########################################################################
+# Annotate genotype information in VCF.
+
+if false; then
+  # Initialize directory.
+  rm -r $path_genotype_dbsnp_annotation
+  mkdir -p $path_genotype_dbsnp_annotation
+
+  path_vcf_source="${path_mayo_bipolar_genotype}/MERGED.maf0.dosR20.3.noDups.chr21.dose.vcf.gz"
+  path_vcf_product="${path_genotype_dbsnp_annotation}/genotype_chromosome_21.vcf.gz"
+
+  report="true"
+
+  # Convert information from genotype files in VCF format to BIM format.
+  /usr/bin/bash "${path_script_dbsnp_rsid_to_vcf}" \
+  $path_dbsnp_reference \
+  $path_vcf_source \
+  $path_vcf_product \
+  $path_bcftools \
+  $report
+fi
 
 ###########################################################################
 # Extract information from VCF to BIM.
@@ -64,13 +90,13 @@ report="true"
 rm -r $path_genotype_snp_bim
 mkdir -p $path_genotype_snp_bim
 
-
 # Temporary Testing...
 
 # Scripts.
 path_script_extract_vcf_to_bim="${path_scripts_utility}/extract_vcf_snps_to_plink_bim.sh"
-path_genotype_source_vcf="${path_genotype_dbsnp_annotation}/genotype_annotation_chromosome_21.vcf.gz"
-name_file_product_bim="genotype_annotation_chromosome_21" # optional to add prefix or suffix here
+path_genotype_source_vcf="${path_genotype_chr_format}/genotype_chromosome_21.vcf.gz"
+#path_genotype_source_vcf="${path_genotype_dbsnp_annotation}/genotype_chromosome_21.vcf.gz"
+name_file_product_bim="genotype_chromosome_21" # optional to add prefix or suffix here
 report="true"
 
 /usr/bin/bash "${path_script_extract_vcf_to_bim}" \
