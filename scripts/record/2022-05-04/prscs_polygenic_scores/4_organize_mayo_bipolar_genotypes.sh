caa1f20442ef1aac6a93fc9ad5c6aac8574e8bd1
T. Cameron Waller <tcameronwaller@gmail.com>
2022-05-17 13:04:10 -0500

    set up test on annotation script

--- scripts/record/2022-05-04/prscs_polygenic_scores/4_organize_mayo_bipolar_genotypes.sh
+++ scripts/record/2022-05-04/prscs_polygenic_scores/4_organize_mayo_bipolar_genotypes.sh
@@ -6,12 +6,13 @@
 #echo "read private file path variables and organize paths..."
 cd ~/paths
 path_bcftools=$(<"./tools_bcftools.txt")
+path_plink2=$(<"./tools_plink2.txt")
 path_process=$(<"./process_psychiatric_metabolism.txt")
 path_dock="${path_process}/dock"
 path_dbsnp_reference="${path_dock}/access/dbsnp_reference/GCF_000001405.39.gz"
 path_mayo_bipolar_genotype="${path_dock}/access/mayo_bipolar_genotype"
 path_genotype_dbsnp_annotation="${path_dock}/access/mayo_bipolar_genotype/dbsnp_annotation"
-path_genotype_product_bim_container="${path_dock}/access/mayo_bipolar_genotype/snp_relevance_bim"
+path_genotype_snp_bim="${path_dock}/access/mayo_bipolar_genotype/snp_relevance_bim"
 
 # Scripts.
 path_promiscuity_scripts="${path_process}/promiscuity/scripts"
@@ -43,8 +44,8 @@ set -x
 rm -r $path_genotype_dbsnp_annotation
 mkdir -p $path_genotype_dbsnp_annotation
 
-path_vcf_source="${path_mayo_bipolar_genotype}/MERGED.maf0.dosR20.3.noDups.chr7.dose.vcf.gz"
-path_vcf_product="${path_genotype_dbsnp_annotation}/genotype_annotation_chromosome_7.vcf.gz"
+path_vcf_source="${path_mayo_bipolar_genotype}/MERGED.maf0.dosR20.3.noDups.chr21.dose.vcf.gz"
+path_vcf_product="${path_genotype_dbsnp_annotation}/genotype_annotation_chromosome_21.vcf.gz"
 
 report="true"
 
@@ -59,15 +60,32 @@ $report
 ###########################################################################
 # Extract information from VCF to BIM.
 
-# Define patterns for file names.
-pattern_genotype_source_vcf_file="MERGED.maf0.dosR20.3.noDups.chr*.dose.vcf.gz" # do not expand with full path yet
+# Initialize directory.
+rm -r $path_genotype_snp_bim
+mkdir -p $path_genotype_snp_bim
+
+
+# Temporary Testing...
+
+# Scripts.
+path_script_extract_vcf_to_bim="${path_scripts_utility}/extract_vcf_to_plink_bim.sh"
+path_genotype_source_vcf="${path_genotype_dbsnp_annotation}/genotype_annotation_chromosome_21.vcf.gz"
+name_file_product_bim="genotype_annotation_chromosome_21" # optional to add prefix or suffix here
 report="true"
+
+/usr/bin/bash "${path_script_extract_vcf_to_bim}" \
+$path_genotype_source_vcf \
+$path_genotype_snp_bim \
+$name_file_product_bim \
+$path_plink2 \
+$report
 
 
 if false; then
-  # Initialize directory.
-  rm -r $path_genotype_product_bim_container
-  mkdir -p $path_genotype_product_bim_container
+
+  # Define patterns for file names.
+  pattern_genotype_source_vcf_file="MERGED.maf0.dosR20.3.noDups.chr*.dose.vcf.gz" # do not expand with full path yet
+  report="true"
 
   # Convert information from genotype files in VCF format to BIM format.
   /usr/bin/bash "${path_script_drive_convert_vcf_to_bim}" \
