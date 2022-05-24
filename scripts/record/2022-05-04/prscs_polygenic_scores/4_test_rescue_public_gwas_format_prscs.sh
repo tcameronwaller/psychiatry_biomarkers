d5690241efdd80eea04ef761a8cc092f8c23778f
T. Cameron Waller <tcameronwaller@gmail.com>
2022-05-24 17:11:21 -0500

    script to format Bipolar Disorder GWAS for analysis in PRS-CS

---
+++ scripts/record/2022-05-04/prscs_polygenic_scores/4_test_rescue_public_gwas_format_prscs.sh
@@ -0,0 +1,47 @@
+#!/bin/bash
+
+###########################################################################
+###########################################################################
+###########################################################################
+# Test procedure or rescue.
+###########################################################################
+###########################################################################
+###########################################################################
+
+
+################################################################################
+# Organize paths.
+# Read private, local file paths.
+cd ~/paths
+path_gwas_summaries=$(<"./gwas_summaries_waller_metabolism.txt")
+path_process=$(<"./process_psychiatric_metabolism.txt")
+path_dock="$path_process/dock"
+
+path_gwas_source="${path_gwas_summaries}/34002096_mullins_2021/daner_bip_pgc3_nm_nomay1.gz"
+path_gwas_product_container="${path_dock}/gwas_format_prscs/34002096_mullins_2021_all_no_mayo"
+
+###########################################################################
+# Execute procedure.
+
+##############################################################################
+# Format GWAS summary statistics for analysis in PRS-CS.
+
+# Initialize directory.
+rm -r $path_gwas_product_container
+mkdir -p $path_gwas_product_container
+
+# Scripts.
+path_promiscuity_scripts="${path_process}/promiscuity/scripts"
+path_script_drive_format_gwas="${path_promiscuity_scripts}/utility/prscs_polygenic_score/drive_format_gwas_prscs.sh"
+path_script_format_gwas="${path_promiscuity_scripts}/gwas_process/format_gwas_prscs/format_gwas_prscs_34002096_mullins_2021_all_no_mayo.sh"
+##########
+# Format adjustment.
+# Parameters.
+report="true" # "true" or "false"
+/usr/bin/bash "${path_script_drive_format_gwas}" \
+$path_gwas_source \
+$path_gwas_product_container \
+$path_script_format_gwas \
+$report
+
+#
