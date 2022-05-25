41a9f5c01413fd3800337ba1ee9525c384b5a0e9
T. Cameron Waller <tcameronwaller@gmail.com>
2022-05-25 11:27:21 -0500

    ready to extract information about SNPs from VCF to BIM

--- scripts/record/2022-05-04/prscs_polygenic_scores/3_organize_mayo_bipolar_genotypes.sh
+++ scripts/record/2022-05-04/prscs_polygenic_scores/3_organize_mayo_bipolar_genotypes.sh
@@ -15,21 +15,26 @@ path_parameters="${path_dock}/parameters"
 path_translations_chromosomes_mayo="${path_parameters}/promiscuity/translations_chromosomes_mayo_bipolar.txt"
 path_human_genome_sequence_compress="${path_dock}/access/human_genome_sequence/grch37/GRCh37.p13.genome.fa.gz"
 path_human_genome_sequence="${path_dock}/access/human_genome_sequence/grch37/GRCh37.p13.genome.fa"
-#path_assembly_translation_chain="${path_dock}/access/human_genome_assembly_chain/ucsc/hg38ToHg19.over.chain.gz"
-path_assembly_translation_chain="${path_dock}/access/human_genome_assembly_chain/ensembl/GRCh38_to_GRCh37.chain.gz"
+host="ucsc"
+#host="ensembl"
+if [[ "$host" == "ucsc" ]]; then
+  path_assembly_translation_chain="${path_dock}/access/human_genome_assembly_chain/ucsc/hg38ToHg19.over.chain.gz"
+elif [[ "$host" == "ensembl" ]]; then
+  path_assembly_translation_chain="${path_dock}/access/human_genome_assembly_chain/ensembl/GRCh38_to_GRCh37.chain.gz"
+else
+  echo "invalid specification of host for genetic reference information"
+fi
 path_dbsnp="${path_dock}/access/dbsnp/grch37_format/GCF_000001405.25.gz"
 path_mayo_bipolar_genotype_raw="${path_dock}/access/mayo_bipolar_genotype_raw"
-#path_mayo_bipolar_genotype_assembly="${path_dock}/access/mayo_bipolar_genotype_grch37_ucsc"
-path_mayo_bipolar_genotype_assembly="${path_dock}/access/mayo_bipolar_genotype_grch37_ensembl"
-#path_mayo_bipolar_genotype_format="${path_dock}/access/mayo_bipolar_genotype_grch37_ucsc_format"
-path_mayo_bipolar_genotype_format="${path_dock}/access/mayo_bipolar_genotype_grch37_ensembl_format"
-path_genotype_snp_relevance_bim="${path_dock}/access/mayo_bipolar_genotype_format/genotype_snp_relevance_bim"
+path_mayo_bipolar_genotype_assembly="${path_dock}/access/mayo_bipolar_genotype_grch37_${host}"
+path_mayo_bipolar_genotype_format="${path_dock}/access/mayo_bipolar_genotype_grch37_${host}_format"
+path_genotype_snp_relevance_bim="${path_mayo_bipolar_genotype_format}/genotype_snp_relevance_bim"
 
 # Scripts.
 path_promiscuity_scripts="${path_process}/promiscuity/scripts"
 path_script_submit_genotype_translate_assembly="${path_promiscuity_scripts}/utility/crossmap/1_submit_batch_directory_all_vcf_assembly_grch38_to_grch37.sh"
 path_script_submit_genotype_format_annotation="${path_promiscuity_scripts}/utility/bcftools/1_submit_batch_directory_all_vcf_format_annotation.sh"
-path_script_drive_extract_vcf_to_bim="${path_promiscuity_scripts}/utility/drive_directory_all_extract_vcf_snps_to_plink_bim.sh"
+path_script_drive_extract_vcf_to_bim="${path_promiscuity_scripts}/utility/plink/drive_directory_all_extract_vcf_snps_to_plink_bim.sh"
 
 ###########################################################################
 # Execute procedure.
@@ -65,7 +70,7 @@ fi
 ###########################################################################
 # Format and annotate genotype information in VCF.
 
-if true; then
+if false; then
   # Initialize directory.
   rm -r $path_mayo_bipolar_genotype_format
   mkdir -p $path_mayo_bipolar_genotype_format
@@ -89,36 +94,21 @@ fi
 ###########################################################################
 # Extract information from VCF to BIM.
 
-if false; then
+if true; then
   # Initialize directory.
   rm -r $path_genotype_snp_relevance_bim
   mkdir -p $path_genotype_snp_relevance_bim
   # Organize specific paths and parameters.
-  path_genotype_source_vcf="${path_genotype_snp_rsid}/genotype_chromosome_21.vcf.gz"
-  name_file_product_bim="genotype_chromosome_21" # optional to add prefix or suffix here
-  threads=10
-  report="true"
-  # Convert information from genotype files in VCF format to BIM format.
-  /usr/bin/bash "${path_script_extract_vcf_to_bim}" \
-  $path_genotype_source_vcf \
-  $path_genotype_snp_bim \
-  $name_file_product_bim \
-  $threads \
-  $path_plink2 \
-  $report
-fi
-
-if false; then
-
-  # Define patterns for file names.
   pattern_genotype_source_vcf_file="snp_MERGED.maf0.dosR20.3.noDups.chr*.dose.vcf.gz" # do not expand with full path yet
+  threads=16
   report="true"
-
   # Convert information from genotype files in VCF format to BIM format.
   /usr/bin/bash "${path_script_drive_convert_vcf_to_bim}" \
   $path_mayo_bipolar_genotype_format \
   $pattern_genotype_source_vcf_file \
   $path_genotype_snp_relevance_bim \
+  $threads \
+  $path_plink2 \
   $path_promiscuity_scripts \
   $report
 fi
