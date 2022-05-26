80fe0663dd079acb98f5e30ba2d865f166f3f412
T. Cameron Waller <tcameronwaller@gmail.com>
2022-05-25 19:05:58 -0500

    ready for genotype format and annotation, second for Ensembl chain

--- scripts/record/2022-05-04/prscs_polygenic_scores/3_organize_mayo_bipolar_genotypes.sh
+++ scripts/record/2022-05-04/prscs_polygenic_scores/3_organize_mayo_bipolar_genotypes.sh
@@ -15,8 +15,8 @@ path_parameters="${path_dock}/parameters"
 path_translations_chromosomes_mayo="${path_parameters}/promiscuity/translations_chromosomes_mayo_bipolar.txt"
 path_human_genome_sequence_compress="${path_dock}/access/human_genome_sequence/grch37/GRCh37.p13.genome.fa.gz"
 path_human_genome_sequence="${path_dock}/access/human_genome_sequence/grch37/GRCh37.p13.genome.fa"
-host="ucsc"
-#host="ensembl"
+#host="ucsc"
+host="ensembl"
 if [[ "$host" == "ucsc" ]]; then
   path_assembly_translation_chain="${path_dock}/access/human_genome_assembly_chain/ucsc/hg38ToHg19.over.chain.gz"
 elif [[ "$host" == "ensembl" ]]; then
@@ -75,7 +75,7 @@ fi
 ###########################################################################
 # Format and annotate genotype information in VCF.
 
-# UCSC chain:
+# UCSC chain: TCW; 25 May 2022; queue
 # Ensembl chain:
 if true; then
   # Initialize directory.
