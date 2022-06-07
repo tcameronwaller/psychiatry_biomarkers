c7a3f1ad5872ff3437b77319755c863eb2901639
T. Cameron Waller <tcameronwaller@gmail.com>
2022-06-06 23:38:10 -0500

    32 threads and 1-day queue for genomic assembly mapping with ENSEMBL chain

--- scripts/record/2022-05-04/prscs_polygenic_scores/3_organize_mayo_bipolar_genotypes.sh
+++ scripts/record/2022-05-04/prscs_polygenic_scores/3_organize_mayo_bipolar_genotypes.sh
@@ -22,8 +22,8 @@ path_mayo_bipolar_genotype_raw="${path_dock}/access/mayo_bipolar_genotype_raw"
 path_directory_mayo_genotype_parent="${path_dock}/genotype_mayo_bipolar_no_decomposition"
 path_directory_genotype_preparation_vcf="${path_directory_mayo_genotype_parent}/preparation_vcf"
 # Mapping from genome assembly GRCh38 to GRCh37.
-host="ucsc"
-#host="ensembl"
+#host="ucsc"
+host="ensembl"
 if [[ "$host" == "ucsc" ]]; then
   path_assembly_translation_chain="${path_dock}/access/human_genome_assembly_chain/ucsc/hg38ToHg19.over.chain.gz"
 elif [[ "$host" == "ensembl" ]]; then
@@ -106,7 +106,7 @@ fi
 # review: TCW; 5 June 2022; I think it's ready here... need to update driver scripts
 
 # without decomposition of multiallelic genetic features
-# UCSC chain: TCW; at  on 06 June 2022; ___;
+# UCSC chain: TCW; at 23:36:10 on 06 June 2022; ___;
 # Ensembl chain: TCW; at  on 06 June 2022; ___;
 ###
 # with decomposition of multiallelic genetic features
