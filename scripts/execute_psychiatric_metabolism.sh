1400e7740a2ea9e864e6daa90248f21f39b53b70
T. Cameron Waller <tcameronwaller@gmail.com>
2022-09-09 15:55:31 -0500

    switch to new procedure to evaluate candidate supplemental phenotype tables

--- scripts/execute_psychiatric_metabolism.sh
+++ scripts/execute_psychiatric_metabolism.sh
@@ -57,9 +57,11 @@ fi
 #python3 $path_package/interface.py uk_biobank --path_dock $path_dock --collection # TCW; 20 April 2022
 
 # Routine: stragglers
+
+python3 $path_package/interface.py stragglers --path_dock $path_dock --mbpdb_prioritize_supplement # TCW; 12 September 2022
 #python3 $path_package/interface.py stragglers --path_dock $path_dock --mbpdb_assembly # TCW; 6 September 2022
 #python3 $path_package/interface.py stragglers --path_dock $path_dock --mbpdb_organization # TCW; 7 September 2022
-python3 $path_package/interface.py stragglers --path_dock $path_dock --mbpdb_regression # TCW; 7 September 2022
+#python3 $path_package/interface.py stragglers --path_dock $path_dock --mbpdb_regression # TCW; 7 September 2022
 #python3 $path_package/interface.py stragglers --path_dock $path_dock --mcita_assembly # TCW; 06 July 2022
 
 ################################################################################
