c6e4eb6b53bd09d5063514026c4c643a493a42f4
T. Cameron Waller <tcameronwaller@gmail.com>
2022-11-17 11:37:40 -0800

    check

---
+++ scripts/record/2022-12-01/6_estimate_gwas_heritability_ldsc.sh
@@ -0,0 +1,130 @@
+#!/bin/bash
+
+###########################################################################
+###########################################################################
+###########################################################################
+# Review: TCW; __ November 2022
+
+###########################################################################
+###########################################################################
+###########################################################################
+
+################################################################################
+# Organize paths.
+
+# Read private, local file paths.
+cd ~/paths
+path_process=$(<"./process_psychiatric_metabolism.txt")
+
+path_dock="$path_process/dock"
+path_directory_source="${path_dock}/bipolar_body/gwas_munge_ldsc"
+path_directory_product="${path_dock}/bipolar_body/gwas_heritability_ldsc"
+path_directory_reference="${path_dock}/bipolar_body/reference_ldsc"
+path_directory_disequilibrium="${path_directory_reference}/disequilibrium/eur_w_ld_chr"
+
+# Scripts.
+path_promiscuity_scripts="${path_process}/promiscuity/scripts"
+path_directory_ldsc="${path_promiscuity_scripts}/utility/ldsc"
+path_script="${path_directory_ldsc}/estimate_gwas_heritability_ldsc.sh"
+
+# Initialize directories.
+rm -r $path_directory_product
+mkdir -p $path_directory_product
+
+################################################################################
+# Organize parameters.
+
+##########
+# Common parameters.
+threads=8
+report="true"
+
+##########
+# Organize multi-dimensional array of information about studies.
+studies=()
+# [regression type] ; [full path to source file of GWAS summary statistics] ; [full path to product file of GWAS summary statistics]
+
+studies+=(
+  "${path_directory_source}/bmi_giant_ukb.sumstats.gz;\
+  ${path_directory_product}/bmi_giant_ukb"
+)
+studies+=(
+  "${path_directory_source}/bmi_giant.sumstats.gz;\
+  ${path_directory_product}/bmi_giant"
+)
+studies+=(
+  "${path_directory_source}/bmi_bipolar_control_ukb.sumstats.gz;\
+  ${path_directory_product}/bmi_bipolar_control_ukb"
+)
+studies+=(
+  "${path_directory_source}/bmi_bipolar_case_pgc_ukb_metasoft.sumstats.gz;\
+  ${path_directory_product}/bmi_bipolar_case_pgc_ukb_metasoft"
+)
+studies+=(
+  "${path_directory_source}/bmi_bipolar_case_pgc_ukb_rma.sumstats.gz;\
+  ${path_directory_product}/bmi_bipolar_case_pgc_ukb_rma"
+)
+studies+=(
+  "${path_directory_source}/bmi_bipolar_case_pgc_ukb_rmafe.sumstats.gz;\
+  ${path_directory_product}/bmi_bipolar_case_pgc_ukb_rmafe"
+)
+studies+=(
+  "${path_directory_source}/bmi_bipolar_case_pgc_ma.sumstats.gz;\
+  ${path_directory_product}/bmi_bipolar_case_pgc_ma"
+)
+studies+=(
+  "${path_directory_source}/bmi_bipolar_case_pgc_mafe.sumstats.gz;\
+  ${path_directory_product}/bmi_bipolar_case_pgc_mafe"
+)
+studies+=(
+  "${path_directory_source}/bmi_bipolar_case_pgc_mafe_fuma.sumstats.gz;\
+  ${path_directory_product}/bmi_bipolar_case_pgc_mafe_fuma"
+)
+studies+=(
+  "${path_directory_source}/bmi_bipolar_case_ukb.sumstats.gz;\
+  ${path_directory_product}/bmi_bipolar_case_ukb"
+)
+
+###########################################################################
+# Execute procedure.
+
+# Organize information for batch instances.
+for study_details in "${studies[@]}"; do
+  # Separate fields from instance.
+  # [regression type] ; [full path to source file of GWAS summary statistics] ; [full path to product file of GWAS summary statistics]
+  IFS=";" read -r -a array <<< "${study_details}"
+  path_file_source="${array[0]}"
+  path_file_base_product="${array[1]}"
+  # Translate GWAS summary statistics to team standard format.
+  /usr/bin/bash "${path_script}" \
+  $path_file_source \
+  $path_file_base_product \
+  $path_file_alleles \
+  $response \
+  $threads \
+  $report
+  # Report.
+  if [[ "$report" == "true" ]]; then
+    echo "----------"
+    echo "Script path:"
+    echo $path_script
+    echo "Source file path:"
+    echo $path_file_source
+    echo "Product file path:"
+    echo $path_file_base_product
+    echo "----------"
+  fi
+done
+
+################################################################################
+# Report.
+if [[ "$report" == "true" ]]; then
+  echo "----------"
+  echo "Script complete:"
+  echo "5_munge_gwas_ldsc.sh"
+  echo "----------"
+fi
+
+
+
+#
