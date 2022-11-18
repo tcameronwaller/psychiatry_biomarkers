2008aeb7527a9cfd78bd11947e2ae856bba8fbd1
T. Cameron Waller <tcameronwaller@gmail.com>
2022-11-18 10:59:47 -0800

    one step format translation

--- scripts/record/2022-12-01/0_test_format_munge_pgc_bmi.sh
+++ scripts/record/2022-12-01/0_test_format_munge_pgc_bmi.sh
@@ -7,9 +7,10 @@
 
 # Notes; TCW; 18 November 2022:
 # 1. LDSC Munge did interpret successfully the SNPs with original identifiers.
+# - - "   print $1, $2, $3, toupper($4), toupper($5), "NA", $7, $8, $9, (3717), "NA", (1), "NA", "NA"   "
 # 2. Original SNP identifier format was "chr[chromosome]_[base-pair position]_[ambiguous allele]".
 # 3. LDSC Munge assigned rsIDs to these SNPs.
-# 4. LDSC kept ___ SNPs from interpretation of original identifiers.
+# 4. LDSC kept 1,217,311 SNPs (1,144,121 non-missing) from interpretation of original identifiers.
 # 5. LDSC Munge did not interpret the SNPs with novel identifiers in format "[chromosome]:[base-pair position]".
 # - - "   split($1, a, "_"); (b = a[1]); sub(/chr/, "", b); print (b ":" a[2]), $2, $3, toupper($4), toupper($5), "NA", $7, $8, $9, (3717), "NA", (1), "NA", "NA"   "
 # 6. LDSC Munge did not interpret the SNPs with novel identifiers in format "[chromosome]_[base-pair position]".
@@ -52,24 +53,26 @@ mkdir -p $path_directory_product
 gunzip -cvf $path_file_gwas_source > $path_file_gwas_source_decompress
 head $path_file_gwas_source_decompress
 
-# One-step format directly to LDSC.
-#echo "SNP A1 A2 N BETA P" > $path_file_gwas_format_ldsc
-#zcat $path_file_gwas_source | awk 'BEGIN { FS=" "; OFS=" " } NR > 1 {print $1, toupper($4), toupper($5), 4332, $7, $9}' >> $path_file_gwas_format
-#head $path_file_gwas_format_ldsc
+if true; then
+  # One-step format directly to LDSC.
+  echo "SNP A1 A2 N BETA P" > $path_file_gwas_format_ldsc
+  zcat $path_file_gwas_source | awk 'BEGIN { FS=" "; OFS=" " } NR > 1 {print $1, toupper($4), toupper($5), (3717), $7, $9}' >> $path_file_gwas_format
+  head $path_file_gwas_format_ldsc
+else
+  # Translation to Team format.
+  echo "SNP CHR BP A1 A2 A1AF BETA SE P N Z INFO NCASE NCONT" > $path_file_gwas_format_team
+  zcat $path_file_gwas_source | awk 'BEGIN {FS = " "; OFS = " "} NR > 1 {
+    print $1, $2, $3, toupper($4), toupper($5), "NA", $7, $8, $9, (3717), "NA", (1), "NA", "NA"
+  }' >> $path_file_gwas_format_team
+  head $path_file_gwas_format_team
 
-# Translation to Team format.
-echo "SNP CHR BP A1 A2 A1AF BETA SE P N Z INFO NCASE NCONT" > $path_file_gwas_format_team
-zcat $path_file_gwas_source | awk 'BEGIN {FS = " "; OFS = " "} NR > 1 {
-  print $1, $2, $3, toupper($4), toupper($5), "NA", $7, $8, $9, (3717), "NA", (1), "NA", "NA"
-}' >> $path_file_gwas_format_team
-head $path_file_gwas_format_team
-
-# Translation to LDSC format.
-echo "SNP A1 A2 N BETA P" > $path_file_gwas_format_ldsc
-cat $path_file_gwas_format_team | awk 'BEGIN { FS=" "; OFS=" " } NR > 1 {
-  print $1, toupper($4), toupper($5), $10, $7, $9
-}' >> $path_file_gwas_format_ldsc
-head $path_file_gwas_format_ldsc
+  # Translation to LDSC format.
+  echo "SNP A1 A2 N BETA P" > $path_file_gwas_format_ldsc
+  cat $path_file_gwas_format_team | awk 'BEGIN { FS=" "; OFS=" " } NR > 1 {
+    print $1, toupper($4), toupper($5), $10, $7, $9
+  }' >> $path_file_gwas_format_ldsc
+  head $path_file_gwas_format_ldsc
+fi
 
 # Munge GWAS summary statistics in LDSC.
 $path_ldsc/munge_sumstats.py \
