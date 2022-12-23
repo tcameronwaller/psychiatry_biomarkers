55d9234f5ffa561166dbb84c0d15556ec60bbf84
T. Cameron Waller <tcameronwaller@gmail.com>
2022-12-23 17:33:09 -0600

    activate full functionality

--- scripts/record/2023-02-01/3_translate_gwas_to_ldsc_format.sh
+++ scripts/record/2023-02-01/3_translate_gwas_to_ldsc_format.sh
@@ -34,8 +34,8 @@ path_directory_promiscuity_scripts="${path_directory_process}/promiscuity/script
 path_file_script="${path_directory_promiscuity_scripts}/gwas_process/format_gwas_ldsc/constrain_translate_gwas_team.sh"
 
 # Initialize directories.
-#rm -r $path_directory_product
-#mkdir -p $path_directory_product
+rm -r $path_directory_product
+mkdir -p $path_directory_product
 
 ################################################################################
 # Organize parameters.
@@ -70,36 +70,26 @@ if [[ "$report" == "true" ]]; then
 fi
 
 for path_file_source in "${paths_files_source[@]}"; do
-  echo $path_file_source
+  # Extract base name of file.
+  name_file_source="$(basename $path_file_source)"
+  path_file_product="${path_directory_product}/${name_file_source}" # hopefully unique
+  # Translate GWAS summary statistics to format for LDSC.
+  /usr/bin/bash "${path_file_script}" \
+  $path_file_source \
+  $path_file_product \
+  $report
+  # Report.
+  if [[ "$report" == "true" ]]; then
+    echo "----------"
+    echo "Script path:"
+    echo $path_file_script
+    echo "Source file path:"
+    echo $path_file_source
+    echo "Product file path:"
+    echo $path_file_product
+    echo "----------"
+  fi
 done
-
-
-################################################################################
-
-if false; then
-  # Iterate on files.
-  for path_file_source in "${paths_files_source[@]}"; do
-    # Extract base name of file.
-    name_file_source="$(basename $path_file_source)"
-    path_file_product="${path_directory_product}/${name_file_source}" # hopefully unique
-    # Translate GWAS summary statistics to format for LDSC.
-    /usr/bin/bash "${path_file_script}" \
-    $path_file_source \
-    $path_file_product \
-    $report
-    # Report.
-    if [[ "$report" == "true" ]]; then
-      echo "----------"
-      echo "Script path:"
-      echo $path_file_script
-      echo "Source file path:"
-      echo $path_file_source
-      echo "Product file path:"
-      echo $path_file_product
-      echo "----------"
-    fi
-  done
-fi
 
 ################################################################################
 # Report.
