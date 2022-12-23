0fa141ac4a7f2028804e02b3411d6b5ef090f16f
T. Cameron Waller <tcameronwaller@gmail.com>
2022-12-23 15:12:16 -0600

    write test draft of driver script for all files in directory

--- scripts/record/2023-02-01/2_translate_gwas_to_standard_format.sh
+++ scripts/record/2023-02-01/2_translate_gwas_to_standard_format.sh
@@ -1,9 +1,27 @@
 #!/bin/bash
 
-###########################################################################
+################################################################################
+################################################################################
+################################################################################
+# Author: T. Cameron Waller
+# Date: 23 December 2022
+################################################################################
+################################################################################
+################################################################################
+# Note
+
+
+
+################################################################################
+################################################################################
+################################################################################
+
+
+
+################################################################################
 # Organize paths.
-# Read private, local file paths.
-#echo "read private file path variables and organize paths..."
+
+# Directories.
 cd ~/paths
 path_bgzip=$(<"./tools_waller_bgzip.txt")
 path_directory_process=$(<"./process_psychiatric_metabolism.txt")
@@ -12,16 +30,18 @@ path_directory_parent_source="${path_directory_gwas_summaries}"
 path_dock="${path_directory_process}/dock"
 path_directory_parameters="${path_dock}/parameters/psychiatric_metabolism"
 path_directory_product="${path_dock}/hormone_genetics/gwas_format_standard"
+# Files.
+path_file_translation="${path_directory_parameters}/table_gwas_translation_tcw_2022-12-23.tsv"
 # Scripts.
 path_promiscuity_scripts="${path_directory_process}/promiscuity/scripts"
 path_directory_script="${path_promiscuity_scripts}/gwas_process/format_gwas_team"
 path_script_drive_translations="${path_promiscuity_scripts}/utility/drive_translations_gwas_to_standard_format.sh"
-# Parameters.
-path_file_translation="${path_directory_parameters}/table_gwas_translation_tcw_2022-12-23.tsv"
 
 # Initialize directories.
 rm -r $path_directory_product
 mkdir -p $path_directory_product
+
+cd $path_directory_product
 
 ###########################################################################
 # Organize parameters.
