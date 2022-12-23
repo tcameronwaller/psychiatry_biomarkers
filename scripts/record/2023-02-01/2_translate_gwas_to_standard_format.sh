6fee0a108b514200f4fdae42e6aaa633dad544f3
T. Cameron Waller <tcameronwaller@gmail.com>
2022-12-23 12:26:23 -0600

    new inclusive parameter table for format translations on GWAS summary statistics

--- scripts/record/2023-02-01/2_translate_gwas_to_standard_format.sh
+++ scripts/record/2023-02-01/2_translate_gwas_to_standard_format.sh
@@ -8,7 +8,7 @@ cd ~/paths
 path_bgzip=$(<"./tools_waller_bgzip.txt")
 path_directory_process=$(<"./process_psychiatric_metabolism.txt")
 path_directory_gwas_summaries=$(<"./gwas_summaries_waller_metabolism.txt")
-path_directory_parent_source="${path_directory_gwas_summaries}/test_test_test"
+path_directory_parent_source="${path_directory_gwas_summaries}"
 path_dock="${path_directory_process}/dock"
 path_directory_parameters="${path_dock}/parameters/psychiatric_metabolism"
 path_directory_product="${path_dock}/hormone_genetics/gwas_format_standard"
@@ -17,7 +17,7 @@ path_promiscuity_scripts="${path_directory_process}/promiscuity/scripts"
 path_directory_script="${path_promiscuity_scripts}/gwas_process/format_gwas_team"
 path_script_drive_translations="${path_promiscuity_scripts}/utility/drive_translations_gwas_to_standard_format.sh"
 # Parameters.
-path_file_translation="${path_directory_parameters}/table_gwas_translation_tcw_2022-12-22_test.tsv"
+path_file_translation="${path_directory_parameters}/table_gwas_translation_tcw_2022-12-23.tsv"
 
 # Initialize directories.
 rm -r $path_directory_product
