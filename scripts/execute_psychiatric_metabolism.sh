4648184b792a6af05905ebdf01b0b14f801c66f2
T. Cameron Waller <tcameronwaller@gmail.com>
2022-12-27 10:21:06 -0600

    estimate LDSC SNP heritability

--- scripts/execute_psychiatric_metabolism.sh
+++ scripts/execute_psychiatric_metabolism.sh
@@ -61,10 +61,10 @@ fi
 
 #python3 $path_package/interface.py stragglers --path_dock $path_dock --scrap # TCW; 05 October 2022
 #python3 $path_package/interface.py stragglers --path_dock $path_dock --mbpdb_prioritize_supplement # TCW; 12 September 2022
-#python3 $path_package/interface.py stragglers --path_dock $path_dock --mbpdb_extraction # TCW; 28 November 2022
+python3 $path_package/interface.py stragglers --path_dock $path_dock --mbpdb_extraction # TCW; 27 December 2022
 #python3 $path_package/interface.py stragglers --path_dock $path_dock --mbpdb_assembly # TCW; 14 September 2022
-python3 $path_package/interface.py stragglers --path_dock $path_dock --mbpdb_organization # TCW; 16 December 2022
-python3 $path_package/interface.py stragglers --path_dock $path_dock --mbpdb_description # TCW; 16 December 2022
+#python3 $path_package/interface.py stragglers --path_dock $path_dock --mbpdb_organization # TCW; 16 December 2022
+#python3 $path_package/interface.py stragglers --path_dock $path_dock --mbpdb_description # TCW; 16 December 2022
 #python3 $path_package/interface.py stragglers --path_dock $path_dock --mbpdb_regression # TCW; 22 September 2022
 #python3 $path_package/interface.py stragglers --path_dock $path_dock --mcita_assembly # TCW; 06 July 2022
 
