a02ff0a89c2d913bf2af1e031a355a43e2d8d961
T. Cameron Waller <tcameronwaller@gmail.com>
2023-03-20 20:39:44 -0500

    include 3 different LD matrix references for SBayesR test

--- scripts/record/2023-03-06_sbayesr_bmi/10_estimate_effect_weights_gctb_sbayesr.sh
+++ scripts/record/2023-03-06_sbayesr_bmi/10_estimate_effect_weights_gctb_sbayesr.sh
@@ -32,18 +32,40 @@ path_directory_reference=$(<"./reference_tcw.txt")
 path_directory_process=$(<"./process_psychiatric_metabolism.txt")
 path_directory_dock="${path_directory_process}/dock" # parent directory for procedural reads and writes
 path_directory_source="${path_directory_dock}/test_sbayesr_body_mass_tcw_2023-03-01/gwas_vcf_process_2"
-path_directory_product="${path_directory_dock}/test_sbayesr_body_mass_tcw_2023-03-01/effect_weights_sbayesr"
-path_directory_ld_matrix="${path_directory_product}/ukbEURu_hm3_shrunk_sparse"
+
+path_directory_product_1="${path_directory_dock}/test_sbayesr_body_mass_tcw_2023-03-21/sbayesr_effects_1"
+path_directory_product_2="${path_directory_dock}/test_sbayesr_body_mass_tcw_2023-03-21/sbayesr_effects_2"
+path_directory_product_3="${path_directory_dock}/test_sbayesr_body_mass_tcw_2023-03-21/sbayesr_effects_3"
+
+path_directory_ld_matrix_1="${path_directory_product_1}/ukbEURu_hm3_shrunk_sparse"
+path_directory_ld_matrix_2="${path_directory_product_2}/ukbEURu_imp_v3_HM3_n50k.chisq10"
+path_directory_ld_matrix_3="${path_directory_product_3}/band_ukb_10k_hm3"
 
 # Files.
 path_file_gwas_source="${path_directory_source}/BMI_GIANTUKB_EUR.txt.gz"
-path_file_gwas_product="${path_directory_product}/BMI_GIANTUKB_EUR.ma"
-path_file_ld_matrix_source="${path_directory_reference}/gctb/ukbEURu_hm3_sparse.zip"
-path_file_ld_matrix_product="${path_directory_product}/ukbEURu_hm3_sparse.zip"
-name_file_ld_matrix_prefix="ukbEURu_hm3_chr"
-name_file_ld_matrix_suffix="_v3_50k.ldm.sparse"
+path_file_gwas_product="${path_directory_product_1}/BMI_GIANTUKB_EUR.ma"
+
+# sbayesr_1_ld_eur_ukb_50k_hm3_ss
+path_file_ld_matrix_1_source="${path_directory_reference}/gctb/ukbEURu_hm3_sparse.zip"
+path_file_ld_matrix_1_product="${path_directory_product_1}/ukbEURu_hm3_sparse.zip"
+name_file_ld_matrix_1_prefix="ukbEURu_hm3_chr"
+name_file_ld_matrix_1_suffix="_v3_50k.ldm.sparse"
+
+# sbayesr_2_ld_eur_ukb_50k_hm3_ss_chi
+path_file_ld_matrix_2_source="${path_directory_reference}/gctb/ukbEURu_imp_v3_HM3_n50k.chisq10.zip"
+path_file_ld_matrix_2_product="${path_directory_product_2}/ukbEURu_imp_v3_HM3_n50k.chisq10.zip"
+name_file_ld_matrix_2_prefix="___"
+name_file_ld_matrix_2_suffix="___"
+
+# sbayesr_3_ld_eur_ukb_10k_hm3_band
+path_file_ld_matrix_3_source="${path_directory_reference}/gctb/band_ukb_10k_hm3.zip"
+path_file_ld_matrix_3_product="${path_directory_product_3}/band_ukb_10k_hm3.zip"
+name_file_ld_matrix_3_prefix="___"
+name_file_ld_matrix_3_suffix="___"
+
+# product
 name_file_product_prefix="BMI_GIANTUKB_EUR_"
-name_file_product_suffix="_tcw_2023-03-01" # Suffix must not be an empty string.
+name_file_product_suffix="_tcw_2023-03-21" # Suffix must not be an empty string.
 
 # Scripts.
 path_script_gwas_format="${path_directory_process}/promiscuity/scripts/gctb/constrain_translate_gwas_standard_to_gctb.sh"
@@ -52,15 +74,20 @@ path_script_batch_run_sbayesr="${path_directory_process}/promiscuity/scripts/gct
 path_script_run_sbayesr="${path_directory_process}/promiscuity/scripts/gctb/run_gctb_sbayesr.sh"
 
 # Initialize directories.
-rm -r $path_directory_product
-mkdir -p $path_directory_product
-cd $path_directory_product
+rm -r $path_directory_product_1
+rm -r $path_directory_product_2
+rm -r $path_directory_product_3
+mkdir -p $path_directory_product_1
+mkdir -p $path_directory_product_2
+mkdir -p $path_directory_product_3
+cd $path_directory_product_1
 
 
 
 ###########################################################################
 # Organize parameters.
 
+observations_variant="1"
 chromosome_x="false"
 threads=1
 report="true"
@@ -89,7 +116,7 @@ fi
 ##########
 # 1. Prepare GWAS summary statistics.
 
-if true; then
+if false; then
   /usr/bin/bash $path_script_gwas_format \
   $path_file_gwas_source \
   $path_file_gwas_product \
@@ -102,8 +129,15 @@ fi
 # 2. Prepare LD matrices.
 
 if true; then
-  cp $path_file_ld_matrix_source $path_file_ld_matrix_product
-  unzip $path_file_ld_matrix_product -d $path_directory_product
+  # 1.
+  cp $path_file_ld_matrix_1_source $path_file_ld_matrix_1_product
+  unzip $path_file_ld_matrix_1_product -d $path_directory_product_1
+  # 2.
+  cp $path_file_ld_matrix_2_source $path_file_ld_matrix_2_product
+  unzip $path_file_ld_matrix_2_product -d $path_directory_product_2
+  # 3.
+  cp $path_file_ld_matrix_3_source $path_file_ld_matrix_3_product
+  unzip $path_file_ld_matrix_3_product -d $path_directory_product_3
 fi
 
 
@@ -111,7 +145,9 @@ fi
 ##########
 # 3. Prepare and submit batch of jobs for processing on each chromosome.
 
-if true; then
+if false; then
+  # 1.
+  cd $path_directory_product_1
   /usr/bin/bash $path_script_submit_batch \
   $path_file_gwas_product \
   $path_directory_ld_matrix \
@@ -120,12 +156,19 @@ if true; then
   $path_directory_product \
   $name_file_product_prefix \
   $name_file_product_suffix \
+  $observations_variant \
   $chromosome_x \
   $path_script_batch_run_sbayesr \
   $path_script_run_sbayesr \
   $path_gctb \
   $threads \
   $report
+
+  # 2.
+  cd $path_directory_product_2
+
+  # 3.
+  cd $path_directory_product_3
 fi
 
 
