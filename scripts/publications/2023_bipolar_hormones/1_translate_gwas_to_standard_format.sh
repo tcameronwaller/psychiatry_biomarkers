81ca818305ec67a8cfba0da881c2f97f033a2531
T. Cameron Waller <tcameronwaller@gmail.com>
2023-03-29 16:30:25 -0500

    test standardization for Neale

--- scripts/publications/2023_bipolar_hormones/1_translate_gwas_to_standard_format.sh
+++ scripts/publications/2023_bipolar_hormones/1_translate_gwas_to_standard_format.sh
@@ -30,9 +30,11 @@ path_directory_gwas_summaries=$(<"./gwas_summaries_waller_metabolism.txt")
 path_directory_parent_source="${path_directory_gwas_summaries}"
 path_directory_dock="${path_directory_process}/dock"
 path_directory_parameters="${path_directory_dock}/parameters/psychiatric_metabolism"
-path_directory_product="${path_directory_dock}/hormone_genetics_tcw_2023-02-24/gwas_format_standard"
+#path_directory_product="${path_directory_dock}/hormone_genetics_tcw_2023-02-24/gwas_format_standard"
+path_directory_product="${path_directory_dock}/hormone_genetics_tcw_2023-02-24/test_test_test_neale"
 # Files.
-path_file_translation="${path_directory_parameters}/table_gwas_translation_tcw_2023-02-24.tsv"
+#path_file_translation="${path_directory_parameters}/table_gwas_translation_tcw_2023-02-24.tsv"
+path_file_translation="${path_directory_parameters}/table_gwas_translation_temp_test_neale_2023-03-29.tsv"
 # Scripts.
 path_directory_promiscuity_scripts="${path_directory_process}/promiscuity/scripts"
 path_directory_script="${path_directory_promiscuity_scripts}/gwas_format/translate_format_raw_to_standard"
