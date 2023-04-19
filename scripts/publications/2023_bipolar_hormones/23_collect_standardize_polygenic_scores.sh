c62c3d3aba0c8b416ab89aae4b826c86cdfd9ba9
T. Cameron Waller <tcameronwaller@gmail.com>
2023-04-19 13:22:09 -0500

    collect and standardize scores

---
+++ scripts/publications/2023_bipolar_hormones/23_collect_standardize_polygenic_scores.sh
@@ -0,0 +1,66 @@
+#!/bin/bash
+
+################################################################################
+# Author: T. Cameron Waller
+# Date, first execution: 21 March 2023
+# Date, last execution: 11 April 2023
+# Review: TCW; 11 April 2023
+################################################################################
+# Note
+
+# Source Format
+# Description: Format polygenic scores standard for collection and standardization.
+# File suffix: ".txt"
+# File type: text
+# File compression: none
+# Delimiter: Tab
+# Columns: identifier   score (TCW; 2023-03-21)
+#          1            2
+
+
+################################################################################
+# Organize paths.
+
+# Directories.
+cd ~/paths
+path_directory_process=$(<"./process_psychiatric_metabolism.txt")
+path_directory_dock="${path_directory_process}/dock" # parent directory for procedural reads and writes
+
+path_directory_source="${path_directory_dock}/hormone_genetics_tcw_2023-02-24/sbayesr_plink_polygenic_scores_preparation"
+path_directory_product="${path_directory_dock}/hormone_genetics_tcw_2023-02-24/sbayesr_plink_polygenic_scores_collection"
+
+# Files.
+name_file_source_prefix="score_" # Must not be empty string.
+name_file_source_suffix=".txt" # Must not be empty string.
+name_file_source_not=".place_holder" # exclude any files that include this character string in file name
+path_file_product="${path_directory_product}/table_scores_collection.tsv"
+
+# Scripts.
+path_script_collect_standardize="${path_directory_process}/promiscuity/scripts/plink/collect_standardize_polygenic_scores.sh"
+
+# Initialize directories.
+rm -r $path_directory_product
+mkdir -p $path_directory_product
+cd $path_directory_product
+
+################################################################################
+# Organize parameters.
+
+
+
+################################################################################
+# Execute procedure.
+
+# Collect and standardize polygenic scores.
+
+/usr/bin/bash $path_script_collect_standardize \
+$path_directory_source \
+$name_file_source_prefix \
+$name_file_source_suffix \
+$name_file_source_not \
+$path_file_product
+
+
+
+
+#
