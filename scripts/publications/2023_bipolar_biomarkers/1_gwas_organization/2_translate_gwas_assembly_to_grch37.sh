7488a36f7864d3b5987ec76c1a86b93ed803d65e
T. Cameron Waller <tcameronwaller@gmail.com>
2023-05-24 16:32:31 -0500

    switch to Ensembl map chain files

--- scripts/publications/2023_bipolar_biomarkers/1_gwas_organization/2_translate_gwas_assembly_to_grch37.sh
+++ scripts/publications/2023_bipolar_biomarkers/1_gwas_organization/2_translate_gwas_assembly_to_grch37.sh
@@ -24,12 +24,12 @@ path_directory_dock="${path_directory_process}/dock" # parent directory for proc
 path_directory_source="${path_directory_dock}/test_crossmap_gwas_summary/gwas_source"
 path_directory_product="${path_directory_dock}/test_crossmap_gwas_summary/gwas_product"
 # Files.
-path_file_chain_ncbi36_to_grch37="${path_directory_reference}/crossmap/ucsc/hg18ToHg19.over.chain.gz"
-path_file_chain_grch37_to_grch38="${path_directory_reference}/crossmap/ucsc/hg19ToHg38.over.chain.gz"
-path_file_chain_grch38_to_grch37="${path_directory_reference}/crossmap/ucsc/hg38ToHg19.over.chain.gz"
-#path_file_chain_ncbi36_to_grch37="${path_directory_reference}/crossmap/ensembl/NCBI36_to_GRCh37.chain.gz"
-#path_file_chain_grch37_to_grch38="${path_directory_reference}/crossmap/ensembl/GRCh37_to_GRCh38.chain.gz"
-#path_file_chain_grch38_to_grch37="${path_directory_reference}/crossmap/ensembl/GRCh38_to_GRCh37.chain.gz"
+#path_file_chain_ncbi36_to_grch37="${path_directory_reference}/crossmap/ucsc/hg18ToHg19.over.chain.gz"
+#path_file_chain_grch37_to_grch38="${path_directory_reference}/crossmap/ucsc/hg19ToHg38.over.chain.gz"
+#path_file_chain_grch38_to_grch37="${path_directory_reference}/crossmap/ucsc/hg38ToHg19.over.chain.gz"
+path_file_chain_ncbi36_to_grch37="${path_directory_reference}/crossmap/ensembl/NCBI36_to_GRCh37.chain.gz"
+path_file_chain_grch37_to_grch38="${path_directory_reference}/crossmap/ensembl/GRCh37_to_GRCh38.chain.gz"
+path_file_chain_grch38_to_grch37="${path_directory_reference}/crossmap/ensembl/GRCh38_to_GRCh37.chain.gz"
 # Scripts.
 path_directory_promiscuity_scripts="${path_directory_process}/promiscuity/scripts"
 path_script_map_assembly="${path_directory_promiscuity_scripts}/crossmap/map_gwas_standard_format_bed.sh"
@@ -48,7 +48,8 @@ report="true"
 ##########
 # 36635386_chen_2023 (GRCh38 to GRCh37)
 
-# Ensembl: 15,428,167 to 15,384,987
+# UCSC: 15,428,167 lines to 15,396,790 lines (TCW; 24 May 2023)
+# Ensembl: 15,428,167 lines to 15,384,987 lines
 /usr/bin/bash $path_script_map_assembly \
 "${path_directory_source}/36635386_chen_2023_cortisol.txt.gz" \
 "${path_directory_product}/36635386_chen_2023_cortisol.txt.gz" \
@@ -86,7 +87,8 @@ $report
 ##########
 # 24586183_medici_2014 (NCBI36-hg18 to GRCh37)
 
-# Ensembl: 2,425,175 to 2,424,987
+# UCSC: 2,425,175 lines to 2,424,714 lines (TCW; 24 May 2023)
+# Ensembl: 2,425,175 lines to 2,424,987 lines
 /usr/bin/bash $path_script_map_assembly \
 "${path_directory_source}/24586183_medici_2014_thyroid_peroxidase_antibody.txt.gz" \
 "${path_directory_product}/24586183_medici_2014_thyroid_peroxidase_antibody.txt.gz" \
--- scripts/publications/2023_bipolar_biomarkers/1_gwas_organization/3_impute_gwas_allele_frequency.sh
+++ scripts/publications/2023_bipolar_biomarkers/1_gwas_organization/3_impute_gwas_allele_frequency.sh
@@ -1,35 +1,15 @@
 #!/bin/bash
 
 ################################################################################
-################################################################################
-################################################################################
 # Author: T. Cameron Waller
 # Date, first execution: 22 February 2023
-# Date, last execution: 27 February 2023
-################################################################################
-################################################################################
+# Date, last execution: 24 May 2023
 ################################################################################
 # Note
 
-# The purpose of this script is to perform any necessary procedures on GWAS
-# summary statistics after completion of the GWAS2VCF procedure.
-# 1. Imputation of frequencies of effect alleles
-
-# Need imputation of allele frequencies
-# 36093044_mathieu_2022
-# 35459240_said_2022
-# 34017140_mbatchou_2021
-# 29875488_sun_2018
-# 24586183_medici_2014
-
-# Do not need imputation of allele frequencies (was just testing)
-# 30367059_teumer_2018
-
-
-################################################################################
-################################################################################
-################################################################################
-
+# The purpose of this script is to impute missing frequencies of effect alleles
+# in GWAS summary statistics from the "European" superpopulation of the
+# 1000 Genomes Project.
 
 
 ################################################################################
@@ -66,23 +46,83 @@ report="true"
 
 cp $path_directory_source/*.txt.gz $path_directory_product
 
-# Perform extra procedures on the sets of GWAS summary statistics for which they
-# are necessary.
+# Impute missing frequencies of effect alleles in GWAS summary statistics.
+
+##########
+# 36093044_mathieu_2022
 
 /usr/bin/bash $path_script_impute_gwas_allele_frequency \
 "${path_directory_source}/36093044_mathieu_2022_hypothyroidism.txt.gz" \
 "${path_directory_product}/36093044_mathieu_2022_hypothyroidism.txt.gz" \
 $report
 
+##########
+# 35459240_said_2022
+
 /usr/bin/bash $path_script_impute_gwas_allele_frequency \
-"${path_directory_source}/30367059_teumer_2018_hypothyroidism.txt.gz" \
-"${path_directory_product}/30367059_teumer_2018_hypothyroidism.txt.gz" \
+"${path_directory_source}/35459240_said_2022_c_reactive_protein.txt.gz" \
+"${path_directory_product}/35459240_said_2022_c_reactive_protein.txt.gz" \
+$report
+
+##########
+# 34017140_mbatchou_2021
+
+/usr/bin/bash $path_script_impute_gwas_allele_frequency \
+"${path_directory_source}/34017140_mbatchou_2021_albumin.txt.gz" \
+"${path_directory_product}/34017140_mbatchou_2021_albumin.txt.gz" \
+$report
+
+##########
+# 29875488_sun_2018
+
+/usr/bin/bash $path_script_impute_gwas_allele_frequency \
+"${path_directory_source}/29875488_sun_2018_complement_c4.txt.gz" \
+"${path_directory_product}/29875488_sun_2018_complement_c4.txt.gz" \
 $report
 
 /usr/bin/bash $path_script_impute_gwas_allele_frequency \
-"${path_directory_source}/30367059_teumer_2018_hyperthyroidism.txt.gz" \
-"${path_directory_product}/30367059_teumer_2018_hyperthyroidism.txt.gz" \
+"${path_directory_source}/29875488_sun_2018_follitropin_fsh.txt.gz" \
+"${path_directory_product}/29875488_sun_2018_follitropin_fsh.txt.gz" \
 $report
+
+/usr/bin/bash $path_script_impute_gwas_allele_frequency \
+"${path_directory_source}/29875488_sun_2018_follistatin.txt.gz" \
+"${path_directory_product}/29875488_sun_2018_follistatin.txt.gz" \
+$report
+
+/usr/bin/bash $path_script_impute_gwas_allele_frequency \
+"${path_directory_source}/29875488_sun_2018_lutropin.txt.gz" \
+"${path_directory_product}/29875488_sun_2018_lutropin.txt.gz" \
+$report
+
+/usr/bin/bash $path_script_impute_gwas_allele_frequency \
+"${path_directory_source}/29875488_sun_2018_lutropin_beta.txt.gz" \
+"${path_directory_product}/29875488_sun_2018_lutropin_beta.txt.gz" \
+$report
+
+/usr/bin/bash $path_script_impute_gwas_allele_frequency \
+"${path_directory_source}/29875488_sun_2018_parathyrin.txt.gz" \
+"${path_directory_product}/29875488_sun_2018_parathyrin.txt.gz" \
+$report
+
+/usr/bin/bash $path_script_impute_gwas_allele_frequency \
+"${path_directory_source}/29875488_sun_2018_thyroid_peroxidase.txt.gz" \
+"${path_directory_product}/29875488_sun_2018_thyroid_peroxidase.txt.gz" \
+$report
+
+##########
+# 24586183_medici_2014
+
+/usr/bin/bash $path_script_impute_gwas_allele_frequency \
+"${path_directory_source}/24586183_medici_2014_thyroid_peroxidase_antibody.txt.gz" \
+"${path_directory_product}/24586183_medici_2014_thyroid_peroxidase_antibody.txt.gz" \
+$report
+
+/usr/bin/bash $path_script_impute_gwas_allele_frequency \
+"${path_directory_source}/24586183_medici_2014_thyroid_peroxidase_reactivity.txt.gz" \
+"${path_directory_product}/24586183_medici_2014_thyroid_peroxidase_reactivity.txt.gz" \
+$report
+
 
 
 #
