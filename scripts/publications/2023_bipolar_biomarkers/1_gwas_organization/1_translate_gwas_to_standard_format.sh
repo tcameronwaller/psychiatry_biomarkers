b0eb2a86203deea1dc47f06b2431018eefa9ba4f
T. Cameron Waller <tcameronwaller@gmail.com>
2023-05-24 11:29:38 -0500

    test procedure to translate GWAS summary statistics to team standard format

--- scripts/publications/2023_bipolar_biomarkers/1_gwas_organization/1_translate_gwas_to_standard_format.sh
+++ scripts/publications/2023_bipolar_biomarkers/1_gwas_organization/1_translate_gwas_to_standard_format.sh
@@ -30,18 +30,19 @@ path_directory_gwas_summaries=$(<"./gwas_summaries_waller_metabolism.txt")
 path_directory_parent_source="${path_directory_gwas_summaries}"
 path_directory_dock="${path_directory_process}/dock"
 path_directory_parameters="${path_directory_dock}/parameters/psychiatric_metabolism"
-#path_directory_product="${path_directory_dock}/hormone_genetics_tcw_2023-02-24/gwas_format_standard"
-path_directory_product="${path_directory_dock}/hormone_genetics_tcw_2023-02-24/test_test_test_neale"
+#path_directory_product="${path_directory_dock}/gwas_biomarkers_tcw_2023-05-24/1_gwas_format_standard"
+path_directory_product="${path_directory_dock}/gwas_biomarkers_tcw_2023-05-24_test/1_gwas_format_standard"
 # Files.
-#path_file_translation="${path_directory_parameters}/table_gwas_translation_tcw_2023-02-24.tsv"
-path_file_translation="${path_directory_parameters}/table_gwas_translation_temp_test_neale_2023-03-29.tsv"
+#path_file_translation="${path_directory_parameters}/table_gwas_translation_tcw_2023-05-23.tsv"
+path_file_translation="${path_directory_parameters}/table_gwas_translation_tcw_2023-05-24_temp_test.tsv"
+
 # Scripts.
 path_directory_promiscuity_scripts="${path_directory_process}/promiscuity/scripts"
 path_directory_script="${path_directory_promiscuity_scripts}/gwas_format/translate_format_raw_to_standard"
 path_script_drive_translations="${path_directory_promiscuity_scripts}/gwas_format/drive_translations_gwas_to_standard_format.sh"
 
 # Initialize directories.
-rm -r $path_directory_product
+rm -r $path_directory_product # caution
 mkdir -p $path_directory_product
 cd $path_directory_product
 
