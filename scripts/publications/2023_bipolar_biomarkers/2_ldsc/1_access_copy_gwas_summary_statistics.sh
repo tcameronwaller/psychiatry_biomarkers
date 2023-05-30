9679cf2849f1c12580b290f4df430cbc04148573
T. Cameron Waller <tcameronwaller@gmail.com>
2023-05-30 13:02:20 -0500

    prepare script to access and copy source collection of GWAS summary statistics

--- scripts/publications/2023_bipolar_biomarkers/2_ldsc/1_access_copy_gwas_summary_statistics.sh
+++ scripts/publications/2023_bipolar_biomarkers/2_ldsc/1_access_copy_gwas_summary_statistics.sh
@@ -8,18 +8,18 @@
 cd ~/paths
 path_directory_gwas_summaries=$(<"./gwas_summaries_waller_metabolism.txt")
 path_directory_gwas="${path_directory_gwas_summaries}/organization"
-path_process=$(<"./process_psychiatric_metabolism.txt")
-path_dock="${path_process}/dock"
+path_directory_process=$(<"./process_psychiatric_metabolism.txt")
+path_directory_dock="${path_directory_process}/dock"
 name_source_directory="5_gwas_effective_observations" # Name of directory with source GWAS summary statistics.
 path_directory_source="${path_directory_gwas}/gwas_biomarkers_tcw_2023-05-25/${name_source_directory}"
-path_directory_product_parent="${path_directory_dock}/gwas_biomarkers_ldsc_tcw_2023-05-25"
+path_directory_product_parent="${path_directory_dock}/ldsc_gwas_biomarkers_tcw_2023-05-25"
 path_directory_product_child="${path_directory_product_parent}/${name_source_directory}"
-path_directory_product="${path_directory_dock}/gwas_biomarkers_ldsc_tcw_2023-05-25/gwas_summaries_source"
+path_directory_product="${path_directory_product_parent}/gwas_summaries_source"
 
 # Initialize directories.
 rm -r $path_directory_product # caution
-mkdir -p $path_directory_product
-cd $path_directory_product
+mkdir -p $path_directory_product_parent
+cd $path_directory_product_parent
 
 ################################################################################
 # Execute procedure.
