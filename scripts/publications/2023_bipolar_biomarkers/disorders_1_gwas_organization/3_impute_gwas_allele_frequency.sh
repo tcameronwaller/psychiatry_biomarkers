48f2f333c7ded26b4f5532dedefd707b0c20f2fd
T. Cameron Waller <tcameronwaller@gmail.com>
2023-08-03 09:02:55 -0400

    ready for process 4

--- scripts/publications/2023_bipolar_biomarkers/disorders_1_gwas_organization/3_impute_gwas_allele_frequency.sh
+++ scripts/publications/2023_bipolar_biomarkers/disorders_1_gwas_organization/3_impute_gwas_allele_frequency.sh
@@ -58,30 +58,36 @@ cp $path_directory_source/*.txt.gz $path_directory_product
 ##########
 # 30482948_walters_2018
 
+# Imputation: 9,225,634 lines to 2,861,129 lines (TCW; 3 August 2023)
 /usr/bin/bash $path_script_impute_gwas_allele_frequency \
 "${path_directory_source}/30482948_walters_2018_eur_all.txt.gz" \
 "${path_directory_product}/30482948_walters_2018_eur_all.txt.gz" \
 $report
 
+# Imputation: 9,271,145 lines to 2,879,377 lines (TCW; 3 August 2023)
 /usr/bin/bash $path_script_impute_gwas_allele_frequency \
 "${path_directory_source}/30482948_walters_2018_eur_unrel_meta.txt.gz" \
 "${path_directory_product}/30482948_walters_2018_eur_unrel_meta.txt.gz" \
 $report
 
+# Imputation: 9,142,832 lines to 4,478,016 lines (TCW; 3 August 2023)
 /usr/bin/bash $path_script_impute_gwas_allele_frequency \
 "${path_directory_source}/30482948_walters_2018_eur_unrel_genotype.txt.gz" \
 "${path_directory_product}/30482948_walters_2018_eur_unrel_genotype.txt.gz" \
 $report
 
+# Imputation: 8,590,974 lines to 4,075,011 lines (TCW; 3 August 2023)
 /usr/bin/bash $path_script_impute_gwas_allele_frequency \
 "${path_directory_source}/30482948_walters_2018_female.txt.gz" \
 "${path_directory_product}/30482948_walters_2018_female.txt.gz" \
 $report
 
+# Imputation: 8,986,505 lines to 4,359,278 lines (TCW; 3 August 2023)
 /usr/bin/bash $path_script_impute_gwas_allele_frequency \
 "${path_directory_source}/30482948_walters_2018_male.txt.gz" \
 "${path_directory_product}/30482948_walters_2018_male.txt.gz" \
 $report
+
 
 
 #
---
+++ scripts/publications/2023_bipolar_biomarkers/disorders_1_gwas_organization/4_clean_gwas_gwas2vcf.sh
@@ -0,0 +1,52 @@
+#!/bin/bash
+
+################################################################################
+# Author: T. Cameron Waller
+# Date, first execution: 23 December 2022
+# Date, last execution: 2 August 2023
+# Date, review: 2 August 2023
+################################################################################
+# Note
+
+
+
+################################################################################
+# Organize paths.
+
+# Directories.
+cd ~/paths
+path_directory_process=$(<"./process_psychiatric_metabolism.txt")
+path_directory_dock="${path_directory_process}/dock"
+path_directory_parameters="${path_directory_dock}/parameters/psychiatric_metabolism"
+
+path_directory_source="${path_directory_dock}/gwas_disorders_tcw_2023-08-02/3_gwas_allele_frequency"
+path_directory_product="${path_directory_dock}/gwas_disorders_tcw_2023-08-02/4_gwas_clean_gwas2vcf"
+# Files.
+path_file_translation="${path_directory_parameters}/table_gwas_translation_tcw_2023-08-02.tsv"
+# Scripts.
+path_directory_partner_scripts="${path_directory_process}/partner/scripts"
+path_script_submit_batch="${path_directory_partner_scripts}/gwas_clean/1_submit_batch_pipe_gwas_clean.sh"
+
+# Initialize directories.
+rm -r $path_directory_product # caution
+mkdir -p $path_directory_product
+cd $path_directory_product
+
+###########################################################################
+# Organize parameters.
+
+report="true"
+
+###########################################################################
+# Execute procedure.
+###########################################################################
+
+/usr/bin/bash $path_script_submit_batch \
+$path_file_translation \
+$path_directory_source \
+$path_directory_product \
+$report
+
+
+
+#
