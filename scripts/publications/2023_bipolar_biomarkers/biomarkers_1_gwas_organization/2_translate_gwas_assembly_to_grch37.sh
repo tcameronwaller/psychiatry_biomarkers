fde441557ab8ccebc4fec1f2ca76069e1fccc928
T. Cameron Waller <tcameronwaller@gmail.com>
2023-09-26 07:20:12 -0400

    new date

--- scripts/publications/2023_bipolar_biomarkers/biomarkers_1_gwas_organization/2_translate_gwas_assembly_to_grch37.sh
+++ scripts/publications/2023_bipolar_biomarkers/biomarkers_1_gwas_organization/2_translate_gwas_assembly_to_grch37.sh
@@ -3,7 +3,7 @@
 ################################################################################
 # Author: T. Cameron Waller
 # Date, first execution: 24 May 2023
-# Date, last execution: 21 September 2023
+# Date, last execution: 25 September 2023
 # Date, review: 19 September 2023
 ################################################################################
 # Note
