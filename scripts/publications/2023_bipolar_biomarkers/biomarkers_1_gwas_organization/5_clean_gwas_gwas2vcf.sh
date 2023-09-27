b1803e456ab0ae2d1d7ab8083679ca24cc536867
T. Cameron Waller <tcameronwaller@gmail.com>
2023-09-27 15:50:40 -0400

    ready for GWAS2VCF process on full batch

--- scripts/publications/2023_bipolar_biomarkers/biomarkers_1_gwas_organization/5_clean_gwas_gwas2vcf.sh
+++ scripts/publications/2023_bipolar_biomarkers/biomarkers_1_gwas_organization/5_clean_gwas_gwas2vcf.sh
@@ -28,13 +28,13 @@ path_directory_dock="${path_directory_process}/dock"
 path_directory_parameters="${path_directory_dock}/parameters/psychiatric_metabolism"
 
 path_directory_source="${path_directory_dock}/gwas_biomarkers_tcw_2023-09-25/4_filter_constrain_gwas_values"
-#path_directory_product="${path_directory_dock}/gwas_biomarkers_tcw_2023-09-25/5_gwas_clean_gwas2vcf"
-path_directory_product="${path_directory_dock}/gwas_biomarkers_tcw_2023-09-25/5_gwas_clean_gwas2vcf_test"
+path_directory_product="${path_directory_dock}/gwas_biomarkers_tcw_2023-09-25/5_gwas_clean_gwas2vcf"
+#path_directory_product="${path_directory_dock}/gwas_biomarkers_tcw_2023-09-25/5_gwas_clean_gwas2vcf_test"
 path_directory_batch="${path_directory_product}/batch"
 
 # Files.
-#path_file_translation="${path_directory_parameters}/table_gwas_translation_tcw_2023-09-19_biomarkers.tsv"
-path_file_translation="${path_directory_parameters}/table_gwas_translation_tcw_2023-09-19_biomarkers_test.tsv"
+path_file_translation="${path_directory_parameters}/table_gwas_translation_tcw_2023-09-19_biomarkers.tsv"
+#path_file_translation="${path_directory_parameters}/table_gwas_translation_tcw_2023-09-19_biomarkers_test.tsv"
 path_file_batch_instances="${path_directory_batch}/batch_instances.txt"
 #path_file_batch_out="${path_directory_product_batch}/batch_out.txt"
 #path_file_batch_error="${path_directory_product_batch}/batch_error.txt"
