5c8b523a6d50db6719abefd23911f9f096e74126
T. Cameron Waller <tcameronwaller@gmail.com>
2023-10-02 09:36:35 -0400

    check and rescue

--- scripts/publications/2023_bipolar_biomarkers/biomarkers_1_gwas_organization/6_check_filter_constrain_gwas_values.sh
+++ scripts/publications/2023_bipolar_biomarkers/biomarkers_1_gwas_organization/6_check_filter_constrain_gwas_values.sh
@@ -4,7 +4,7 @@
 # Author: T. Cameron Waller
 # Date, first execution: 2 October 2023
 # Date, last execution: 2 October 2023
-# Date, review: __ October 2023
+# Date, review: 2 October 2023
 ################################################################################
 # Note
 
@@ -18,8 +18,11 @@ path_directory_process=$(<"./process_psychiatric_metabolism.txt")
 path_directory_parent_temporary="${path_directory_process}/temporary_check_6"
 path_directory_dock="${path_directory_process}/dock"
 path_directory_parameters="${path_directory_dock}/parameters/psychiatric_metabolism"
+
 path_directory_source="${path_directory_dock}/gwas_biomarkers_tcw_2023-09-29/5_gwas_clean_gwas2vcf"
+path_directory_source_rescue="${path_directory_dock}/gwas_biomarkers_tcw_2023-09-29/4_filter_constrain_gwas_values"
 path_directory_product="${path_directory_dock}/gwas_biomarkers_tcw_2023-09-29/6_filter_constrain_gwas_values"
+
 # Files.
 # Scripts.
 path_directory_partner_scripts="${path_directory_process}/partner/scripts"
@@ -41,16 +44,12 @@ report="true"
 ################################################################################
 # Execute procedure.
 
-##########
-# Copy the GWAS summary statistics from the previous process.
-# Most sets of GWAS summary statistics do not need extra processing.
-# Subsequent processes on a few studies will replace the appropriate files.
-cp $path_directory_source/*.txt.gz $path_directory_product
+
 
 ##########
 # 36376304_koskeridis_2022_c_reactive_protein
 
-# records that do not pass checks: 3 of 6,206,409 (header line); most due to values out of range in probability (p-value)
+# records that do not pass checks: 1 of 6,205,745 (header line)
 /usr/bin/bash $path_script_check \
 "${path_directory_source}/36376304_koskeridis_2022_c_reactive_protein.txt.gz" \
 $path_directory_parent_temporary \
@@ -59,7 +58,7 @@ $report
 ##########
 # 34662886_backman_2021_albumin
 
-# records that do not pass checks: 1 of 499,614 (header line)
+# records that do not pass checks: 1 of 444,842 (header line)
 /usr/bin/bash $path_script_check \
 "${path_directory_source}/34662886_backman_2021_albumin.txt.gz" \
 $path_directory_parent_temporary \
@@ -68,7 +67,7 @@ $report
 ##########
 # 34226706_barton_2021_albumin
 
-# records that do not pass checks: 246,495 of 5,515,076 (header line); most due to missingness in effect and standard error
+# records that do not pass checks: 1 of 4,746,640 (header line)
 /usr/bin/bash $path_script_check \
 "${path_directory_source}/34226706_barton_2021_albumin.txt.gz" \
 $path_directory_parent_temporary \
@@ -77,7 +76,7 @@ $report
 ##########
 # 32769997_zhou_2020
 
-# records that do not pass checks: 2,993 of 22,397,081 (header line); all due to missingness in count of observations
+# records that do not pass checks: 1 of 21,795,381 (header line)
 /usr/bin/bash $path_script_check \
 "${path_directory_source}/32769997_zhou_2020_thyroid_hormone.txt.gz" \
 $path_directory_parent_temporary \
@@ -86,19 +85,25 @@ $report
 ##########
 # 32581359_saevarsdottir_2020
 
-# records that raise checks: 1 of 43,529,207 (header line)
+# records that raise checks: 1 of 1 (header line)
 /usr/bin/bash $path_script_check \
 "${path_directory_source}/32581359_saevarsdottir_2020_thyroid_autoimmunity.txt.gz" \
 $path_directory_parent_temporary \
 $report
 
-# records that raise checks: 1 of 17,412,123 (header line)
-# records with allele frequency of zero: 7,106,273 (header line)
+# records that raise checks: 1 of 17,411,881 (header line)
 /usr/bin/bash $path_script_check \
 "${path_directory_source}/32581359_saevarsdottir_2020_thyroid_autoimmunity_af_impute.txt.gz" \
 $path_directory_parent_temporary \
 $report
 
+
+
+##########
+# Copy the GWAS summary statistics from the previous process.
+# Most sets of GWAS summary statistics do not need extra processing.
+# Subsequent processes on a few studies will replace the appropriate files.
+cp $path_directory_source/*.txt.gz $path_directory_product
 
 
 
@@ -107,6 +112,20 @@ $report
 
 # 32581359_saevarsdottir_2020_thyroid_autoimmunity.txt.gz
 # 32242144_revez_2020_vitamin_d.txt.gz
+
+rm $path_directory_product/32581359_saevarsdottir_2020_thyroid_autoimmunity.txt.gz
+rm $path_directory_product/32242144_revez_2020_vitamin_d.txt.gz
+
+
+
+##########
+# Copy GWAS summary statistics before GWAS2VCF where appropriate.
+
+# Note: It has been difficult to determine why GWAS2VCF failed to process the
+# GWAS summary statistics for the study below.
+# 32581359_saevarsdottir_2020_thyroid_autoimmunity
+
+cp $path_directory_source_rescue/32581359_saevarsdottir_2020_thyroid_autoimmunity.txt.gz $path_directory_product
 
 
 
