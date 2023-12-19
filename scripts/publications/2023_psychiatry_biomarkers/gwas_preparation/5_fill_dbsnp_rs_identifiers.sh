080ac51e45f550dbc89a833bcd99c9af307ac678
T. Cameron Waller <tcameronwaller@gmail.com>
2023-12-19 18:09:11 -0500

    simplify for testing

--- scripts/publications/2023_psychiatry_biomarkers/gwas_preparation/5_fill_dbsnp_rs_identifiers.sh
+++ scripts/publications/2023_psychiatry_biomarkers/gwas_preparation/5_fill_dbsnp_rs_identifiers.sh
@@ -75,41 +75,41 @@ studies=()
 studies+=("37872160_williams_2023")
 #studies+=("36635386_chen_2023_cortisol") # Unnecessary. Supplement.
 #studies+=("36635386_chen_2023_thyroxine_total") # Unnecessary. Supplement.
-studies+=("34662886_backman_2021_albumin")
+#studies+=("34662886_backman_2021_albumin")
 
 #studies+=("34594039_sakaue_2021_multi_hypothyroidism")
 #studies+=("34594039_sakaue_2021_multi_hyperthyroidism")
 #studies+=("34594039_sakaue_2021_multi_hashimoto")
 #studies+=("34594039_sakaue_2021_multi_graves")
-studies+=("34594039_sakaue_2021_eur_hypothyroidism")
-studies+=("34594039_sakaue_2021_eur_hyperthyroidism")
-studies+=("34594039_sakaue_2021_eur_hashimoto")
-studies+=("34594039_sakaue_2021_eur_graves")
+#studies+=("34594039_sakaue_2021_eur_hypothyroidism")
+#studies+=("34594039_sakaue_2021_eur_hyperthyroidism")
+#studies+=("34594039_sakaue_2021_eur_hashimoto")
+#studies+=("34594039_sakaue_2021_eur_graves")
 #studies+=("34594039_sakaue_2021_eur_rheumatoid_arthritis")
 #studies+=("34594039_sakaue_2021_gc_hypothyroidism")
 #studies+=("34594039_sakaue_2021_gc_hyperthyroidism")
 #studies+=("34594039_sakaue_2021_gc_hashimoto")
 #studies+=("34594039_sakaue_2021_gc_graves")
 
-studies+=("34226706_barton_2021_albumin")
+#studies+=("34226706_barton_2021_albumin")
 studies+=("34017140_mbatchou_2021_albumin")
 
-studies+=("33587031_sinnott-armstrong_2021_testosterone_primary_female")
-studies+=("33587031_sinnott-armstrong_2021_testosterone_primary_male")
-studies+=("33587031_sinnott-armstrong_2021_lutropin")
+#studies+=("33587031_sinnott-armstrong_2021_testosterone_primary_female")
+#studies+=("33587031_sinnott-armstrong_2021_testosterone_primary_male")
+#studies+=("33587031_sinnott-armstrong_2021_lutropin")
 
 studies+=("32769997_zhou_2020_thyroid_hormone")
 #studies+=("32581359_saevarsdottir_2020_thyroid_autoimmunity") # Unnecessary. Supplement.
 #studies+=("32059762_manousaki_2020_vitamin_d")
 
-studies+=("30367059_teumer_2018_thyroid_hormone_all")
+#studies+=("30367059_teumer_2018_thyroid_hormone_all")
 studies+=("30367059_teumer_2018_thyroid_hormone_female")
-studies+=("30367059_teumer_2018_thyroid_hormone_male")
-studies+=("30367059_teumer_2018_thyroxine_free_all")
-studies+=("30367059_teumer_2018_thyroxine_free_female")
-studies+=("30367059_teumer_2018_thyroxine_free_male")
-studies+=("30367059_teumer_2018_hypothyroidism")
-studies+=("30367059_teumer_2018_hyperthyroidism")
+#studies+=("30367059_teumer_2018_thyroid_hormone_male")
+#studies+=("30367059_teumer_2018_thyroxine_free_all")
+#studies+=("30367059_teumer_2018_thyroxine_free_female")
+#studies+=("30367059_teumer_2018_thyroxine_free_male")
+#studies+=("30367059_teumer_2018_hypothyroidism")
+#studies+=("30367059_teumer_2018_hyperthyroidism")
 
 #studies+=("29875488_sun_2018_complement_c4")
 #studies+=("29875488_sun_2018_follitropin")
@@ -118,6 +118,23 @@ studies+=("30367059_teumer_2018_hyperthyroidism")
 #studies+=("29875488_sun_2018_lutropin_beta")
 #studies+=("29875488_sun_2018_parathyrin")
 #studies+=("29875488_sun_2018_thyroid_peroxidase")
+
+
+
+##########
+# Execute process iteratively for each study.
+if false; then
+  for study in "${studies[@]}"; do
+    path_file_source="${path_directory_source}/${study}.txt.gz"
+    path_file_product="${path_directory_product}/${study}.txt.gz"
+    # Call script.
+    /usr/bin/bash $path_file_script_fill_dbsnp_rsid \
+    $path_file_source \
+    $path_file_product \
+    $strict \
+    $report
+  done
+fi
 
 
 ##########
