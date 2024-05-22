69676a77a648ee7060fd341a0d9665e42958dcf4
T. Cameron Waller <tcameronwaller@gmail.com>
2024-05-22 14:41:21 -0500

    update directory structure for source and product files

--- scripts/publications/2024_psychiatry_biomarkers/gwas_analysis_ldsc/7_extract_ldsc_heritability_correlation.sh
+++ scripts/publications/2024_psychiatry_biomarkers/gwas_analysis_ldsc/7_extract_ldsc_heritability_correlation.sh
@@ -19,10 +19,11 @@
 ################################################################################
 # Organize paths.
 
-# Identifiers or designators of parameter version, preparation batch, and
-# analysis batch.
-identifier_analysis="gwas_2023-12-30_ldsc_2024-01-08_extra_2024-05-15"
+# Identifiers or designators of groups.
+identifier_preparation="gwas_2023-12-30_ldsc_2024-01-08_extra_2024-05-15"
+identifier_analysis="analysis_primary_secondary_2024-01-08_2024-05-16"
 identifier_parameter="tcw_2023-12-30_dbsnp_rsid"
+identifier_extraction="extraction_2024-05-22"
 
 # Directories.
 cd ~/paths
@@ -32,29 +33,28 @@ path_directory_process=$(<"./process_psychiatric_metabolism.txt")
 path_directory_temporary="${path_directory_process}/_temporary_blargish_tcw_2024-05-17_"
 path_directory_dock="${path_directory_process}/dock"
 
-path_directory_group_parent="${path_directory_dock}/${identifier_analysis}"
+path_directory_group_parent="${path_directory_dock}/${identifier_preparation}"
 
-path_directory_source_h2_1="${path_directory_group_parent}/5_gwas_heritability_ldsc"
-path_directory_product_h2_1="${path_directory_group_parent}/extraction/5_gwas_heritability_ldsc"
+path_directory_source_h2_1="${path_directory_group_parent}/${identifier_analysis}/5_gwas_heritability_ldsc"
+path_directory_product_h2_1="${path_directory_group_parent}/${identifier_extraction}/5_gwas_heritability_ldsc"
 #
-path_directory_source_h2_2="${path_directory_group_parent}/5_gwas_heritability_ldsc_no_liability"
-path_directory_product_h2_2="${path_directory_group_parent}/extraction/5_gwas_heritability_ldsc_no_liability"
+path_directory_source_h2_2="${path_directory_group_parent}/${identifier_analysis}/5_gwas_heritability_ldsc_no_liability"
+path_directory_product_h2_2="${path_directory_group_parent}/${identifier_extraction}/5_gwas_heritability_ldsc_no_liability"
 
-#path_directory_source_rg="${path_directory_group_parent}/6_gwas_correlation_ldsc_all"
-#path_directory_product_rg="${path_directory_group_parent}/6_gwas_correlation_ldsc_all_extraction"
+#path_directory_source_rg="${path_directory_group_parent}/${identifier_analysis}/6_gwas_correlation_ldsc_all"
+#path_directory_product_rg="${path_directory_group_parent}/${identifier_extraction}/6_gwas_correlation_ldsc_all"
 #
-path_directory_source_rg_1="${path_directory_group_parent}/6_gwas_correlation_ldsc_primary"
-path_directory_product_rg_1="${path_directory_group_parent}/extraction/6_gwas_correlation_ldsc_primary"
+path_directory_source_rg_1="${path_directory_group_parent}/${identifier_analysis}/6_gwas_correlation_ldsc_primary"
+path_directory_product_rg_1="${path_directory_group_parent}/${identifier_extraction}/6_gwas_correlation_ldsc_primary"
 #
-path_directory_source_rg_2="${path_directory_group_parent}/6_gwas_correlation_ldsc_secondary"
-path_directory_product_rg_2="${path_directory_group_parent}/extraction/6_gwas_correlation_ldsc_secondary"
+path_directory_source_rg_2="${path_directory_group_parent}/${identifier_analysis}/6_gwas_correlation_ldsc_secondary"
+path_directory_product_rg_2="${path_directory_group_parent}/${identifier_extraction}/6_gwas_correlation_ldsc_secondary"
 #
-path_directory_source_rg_3="${path_directory_group_parent}/6_gwas_correlation_ldsc_primary_secondary"
-path_directory_product_rg_3="${path_directory_group_parent}/extraction/6_gwas_correlation_ldsc_primary_secondary"
+path_directory_source_rg_3="${path_directory_group_parent}/${identifier_analysis}/6_gwas_correlation_ldsc_secondary_thyroid"
+path_directory_product_rg_3="${path_directory_group_parent}/${identifier_extraction}/6_gwas_correlation_ldsc_secondary_thyroid"
 #
-path_directory_source_rg_4="${path_directory_group_parent}/6_gwas_correlation_ldsc_secondary_thyroid"
-path_directory_product_rg_4="${path_directory_group_parent}/extraction/6_gwas_correlation_ldsc_secondary_thyroid"
-
+path_directory_source_rg_4="${path_directory_group_parent}/${identifier_analysis}/6_gwas_correlation_ldsc_primary_secondary"
+path_directory_product_rg_4="${path_directory_group_parent}/${identifier_extraction}/6_gwas_correlation_ldsc_primary_secondary"
 
 # Scripts.
 path_file_script_extract_h2_rg="${path_directory_process}/partner/scripts/ldsc/extract_ldsc_heritability_correlation.sh"
@@ -136,13 +136,13 @@ if false; then
   name_file_product="none"
   /usr/bin/bash $path_file_script_extract_h2_rg \
   $type_analysis \
-  $path_directory_source_rg_4 \
+  $path_directory_source_rg_3 \
   $traversal \
   $name_file_source_prefix \
   $name_file_source_suffix \
   $name_file_source_not \
   $name_file_product \
-  $path_directory_product_rg_4 \
+  $path_directory_product_rg_3 \
   $path_directory_process \
   $path_directory_temporary \
   $path_directory_environment \
@@ -205,6 +205,25 @@ if false; then
   $name_file_source_not \
   $name_file_product \
   $path_directory_product_rg_3 \
+  $path_directory_process \
+  $path_directory_temporary \
+  $path_directory_environment \
+  $report
+
+  ##########
+  # Genetic correlation.
+  type_analysis="correlation"
+  traversal="true" # whether to extract from all files in child source directories, preserving names of child directories
+  name_file_product="none"
+  /usr/bin/bash $path_file_script_extract_h2_rg \
+  $type_analysis \
+  $path_directory_source_rg_4 \
+  $traversal \
+  $name_file_source_prefix \
+  $name_file_source_suffix \
+  $name_file_source_not \
+  $name_file_product \
+  $path_directory_product_rg_4 \
   $path_directory_process \
   $path_directory_temporary \
   $path_directory_environment \
